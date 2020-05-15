3e72402acd1c185b93cec9e7b852d4f572cb9065
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-05-15 07:19:06 -0700

    style updates

--- src/App/Register/styles.js
+++ src/App/Register/styles.js
@@ -12,11 +12,25 @@ export const
 	},
 	modalContainer = {
 		display: 'grid',
-		marginBottom: '15px',
-		textAlign: 'start'
+		margin: '30px 0 15px',
+		textAlign: 'center'
 	},
 	modalLabel = {
+		display: 'flex',
+		justifyContent: 'center',
 		fontFamily: fontTitle,
 		fontSize: fontSizeSmall,
 		textTransform: 'uppercase'
+	},
+	modalBox = {
+		zIndex: '999',
+		maxWidth: '500px',
+		width: '90%',
+		margin: '0 auto',
+		padding: '5%',
+		boxSizing: 'border-box',
+		borderRadius: '3px',
+		background: 'white',
+		boxShadow: `1px 0px 8px 0px rgba(34,34,34,0.15), 1px 0px 8px 0px rgba(34,34,34,0.10),
+		1px 0px 8px 0px rgba(34,34,34,0.05)`
 	}
