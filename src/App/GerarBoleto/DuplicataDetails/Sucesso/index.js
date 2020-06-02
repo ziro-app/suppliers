e064268e9e3333ed4b9d8d511dad76802b194b9d
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-06-02 16:52:00 -0700

    fixes

--- src/App/GerarBoleto/DuplicataDetails/Sucesso/index.js
+++ src/App/GerarBoleto/DuplicataDetails/Sucesso/index.js
@@ -2,13 +2,12 @@ import React from 'react'
 import { useLocation } from 'wouter'
 import Button from '@bit/vitorbarbosa19.ziro.button'
 import Illustration from '@bit/vitorbarbosa19.ziro.illustration'
-import { containerWithPadding } from '@ziro/theme'
 import { container,svg, title } from './styles'
 
-const ErrorExpired = ({urlBoleto}) => {
+const Sucesso = ({urlBoleto}) => {
 	const [, setLocation] = useLocation()
 	return (
-		<div style={{...containerWithPadding, ...container}}>
+		<div style={container}>
 			<div style={svg}><Illustration type='paymentSuccess' /></div>
 			<label style={title}>Duplicata gerada com sucesso</label>
 			<Button type='link' cta='Visualizar Duplicata' navigate={async () => window.open(urlBoleto,'_blank')} />
@@ -20,4 +19,4 @@ const ErrorExpired = ({urlBoleto}) => {
 	)
 }
 
-export default ErrorExpired
+export default Sucesso
--- src/App/GerarBoleto/DuplicataDetails/Sucesso/styles.js
+++ src/App/GerarBoleto/DuplicataDetails/Sucesso/styles.js
@@ -4,8 +4,11 @@ export const
 
 container = {
 	display: 'grid',
-	alignContent: 'center',
 	gridRowGap: '15px',
+	maxWidth: '500px',
+	boxSizing: 'border-box',
+	margin: '0 auto',
+	padding: '20px 20px 60px',
 	color: primaryColor,
 	textAlign: 'center'
 },
