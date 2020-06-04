5fe2269f3f4d4d595658ab2e519ac4df182d59fe
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-06-04 10:31:39 -0300

    Added photo rotation

--- src/App/GerarBoleto/BoletoDetails/index.js
+++ src/App/GerarBoleto/BoletoDetails/index.js
@@ -7,22 +7,25 @@ import Modal from '@bit/vitorbarbosa19.ziro.modal';
 import currencyFormat from '@ziro/currency-format';
 import { formatDateUTC3 } from '@ziro/format-date-utc3'
 import Button from '@bit/vitorbarbosa19.ziro.button';
+import CameraButton from '@bit/vitorbarbosa19.ziro.camera-button';
 import NotFound from './NotFound/index'
 import { modalContainer } from './styles'
 
-export default ({boletbankId,boletId,data}) => {
+export default ({ boletbankId, boletId, data }) => {
     const filtrado = data.values.filter(item => {
-        if(item.boleto){
+        if (item.boleto) {
             return String(item.boleto) === boletId
-        }else{
+        } else {
             return String(item.boletId) === boletId
         }
     })
     const [blocks, setBlocks] = useState([]);
     const [isOpen, setIsOpen] = useState(false);
     const [linkImage, setLinkImage] = useState('')
+    const [degrees, setDegrees] = useState(90)
+    const [counter, setCounter] = useState(0)
     useEffect(() => {
-        const {comissao, lojista, polo, receita, romaneio, rua, valor, vencimento, venda,boleto,boletId,data_venda,url} = filtrado[0]
+        const { comissao, lojista, polo, receita, romaneio, rua, valor, vencimento, venda, boleto, boletId, data_venda, url } = filtrado[0]
         let block;
         block = [
             {
@@ -66,23 +69,36 @@ export default ({boletbankId,boletId,data}) => {
         setBlocks(block)
         setLinkImage(url)
     }, [])
+    useEffect(() => setDegrees(degrees - 90), [counter])
     const textAreaRef = useRef(null);
     return (
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} style={containerWithPadding}>
-                <input type="text" style={{ position: 'absolute', left: '-9999px' }} ref={textAreaRef} readOnly />
-                <Header type='icon-link' title={`Boleto ${boletId || boleto}`} navigateTo={`relatorio/${boletbankId}`} icon='back' />
-                <div style={{ display: 'grid', gridRowGap: '12px'}}>
-                    <Details blocks={blocks} />
-                    <Modal boxStyle={modalContainer} isOpen={isOpen} setIsOpen={() => setIsOpen(false)}>
+            <input type="text" style={{ position: 'absolute', left: '-9999px' }} ref={textAreaRef} readOnly />
+            <Header type='icon-link' title={`Boleto ${boletId || boleto}`} navigateTo={`relatorio/${boletbankId}`} icon='back' />
+            <div style={{ display: 'grid', gridRowGap: '12px' }}>
+                <Details blocks={blocks} />
+                <Modal degrees={degrees} boxStyle={modalContainer} isOpen={isOpen} setIsOpen={() => setIsOpen(false)}>
                     {linkImage
-                        ? <img style={{display: 'block', width:'100%'}} src={linkImage} alt={'Imagem do Boleto'}/>
+                        ? <div style={{ position: 'relative' }}>
+                            <img style={{ display: 'block', width: '100%' }} src={linkImage} alt={'Imagem do Boleto'} />
+                            <div style={{
+                                position: 'absolute',
+                                overflow: 'hidden',
+                                top: 0,
+                                bottom: 0,
+                                left: 0,
+                                right: 0
+                            }}>
+                                <CameraButton size={30} click={() => setCounter(counter + 1)} type='swap' />
+                            </div>
+                        </div>
                         : <NotFound />
                     }
-                    </Modal>
-                     <div style={{ display: 'grid', marginTop:'15px' }} onClick={() => setIsOpen(true)}>
+                </Modal>
+                <div style={{ display: 'grid', marginTop: '15px' }} onClick={() => setIsOpen(true)}>
                     <Button type="submit" cta="Imagem do boleto" />
                 </div>
-                </div>
-            </motion.div>
+            </div>
+        </motion.div>
     )
 }
