80c774cff24f1d6b277ca464a1ef883b60d3a91b
Alessandro <vyseclown@gmail.com>
2020-06-11 00:24:10 -0300

    Just changing from transacao to pagamento

--- src/App/CreatePayment/sendToBackend.js
+++ src/App/CreatePayment/sendToBackend.js
@@ -1,46 +1,45 @@
-import { db } from '../../Firebase/index'
+import { db } from '../../Firebase/index';
 
 const sendToBackend = state => () => {
-    const { seller, sellerId, charge, maxInstallments, setCharge, setMaxInstallments } = state
-    const baseUrl = 'https://ziro.app/transacao?doc='
-    return new Promise(async (resolve, reject) => {
+  const { seller, sellerId, charge, maxInstallments, setCharge, setMaxInstallments } = state;
+  const baseUrl = 'https://ziro.app/pagamento/';
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (seller && sellerId) {
+        const docRef = await db.collection('credit-card-payments').add({
+          dateLinkCreated: new Date(),
+          seller,
+          sellerZoopId: sellerId,
+          charge,
+          maxInstallments,
+          status: 'Aguardando Pagamento',
+        });
         try {
-            if (seller && sellerId) {
-                const docRef = await db.collection('credit-card-payments').add({
-                    dateLinkCreated: new Date(),
-                    seller,
-                    sellerZoopId: sellerId,
-                    charge,
-                    maxInstallments,
-                    status: 'Aguardando Pagamento'
-                })
-                try {
-                    const doc = await docRef.get()
-                    if (doc) await navigator.clipboard.writeText(`${baseUrl}${doc.id}`)
-                } catch (error) {
-                    throw { msg: 'Erro ao realizar a cópia', copyError: true }
-                }
-                resolve('Link copiado')
-                setCharge('')
-                setMaxInstallments('')
-            } else {
-                throw { msg: 'Vendedor não encontrado', customError: true }
-            }
+          const doc = await docRef.get();
+          if (doc) await navigator.clipboard.writeText(`${baseUrl}${doc.id}/escolher-cartao`);
         } catch (error) {
-            if (error.copyError) {
-                resolve('Link criado no menu de Vendas')
-                setCharge('')
-                setMaxInstallments('')
-            }
-            if (error.customError) reject(error)
-            else {
-                console.log(error)
-                if (error.response) console.log(error.response)
-                reject('Erro ao criar cobrança')
-            }
+          throw { msg: 'Erro ao realizar a cópia', copyError: true };
         }
-    })
+        resolve('Link copiado');
+        setCharge('');
+        setMaxInstallments('');
+      } else {
+        throw { msg: 'Vendedor não encontrado', customError: true };
+      }
+    } catch (error) {
+      if (error.copyError) {
+        resolve('Link criado no menu de Vendas');
+        setCharge('');
+        setMaxInstallments('');
+      }
+      if (error.customError) reject(error);
+      else {
+        console.log(error);
+        if (error.response) console.log(error.response);
+        reject('Erro ao criar cobrança');
+      }
+    }
+  });
+};
 
-}
-
-export default sendToBackend
+export default sendToBackend;
