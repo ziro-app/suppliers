f27666037b5be388167a54d3dc037fb777bd6a9f
Alessandro <vyseclown@gmail.com>
2020-06-29 17:04:02 -0300

    Fixing the receipt error when the transaction are out of the list of 10

--- src/App/Transactions/ReceivableDetails/index.js
+++ src/App/Transactions/ReceivableDetails/index.js
@@ -8,74 +8,93 @@ import currencyFormat from '@ziro/currency-format';
 import { containerWithPadding } from '@ziro/theme';
 import matchStatusColor from '../matchStatusColor';
 import { dateFormat, parcelFormat, round } from '../utils';
+import fetch from '../TransactionDetails/fetch';
 
-const ReceivableDetails = ({ transactions, transactionId, receivableId }) => {
-    const [blocks, setBlocks] = useState([]);
-    const [transaction, setTransaction] = useState({});
-    const [receivable, setReceivable] = useState({});
-    const [, setLocation] = useLocation();
+const ReceivableDetails = ({ transactions, transactionId, receivableId, transaction, setTransaction }) => {
+  const [blocks, setBlocks] = useState([]);
+  //const [transaction, setTransaction] = useState({});
+  const [error, setError] = useState();
+  const [receivable, setReceivable] = useState({});
+  const [, setLocation] = useLocation();
 
-    useEffect(() => {
-        const effectTransaction = transactions.filter(transaction => transaction.transactionId === transactionId)[0];
-        setTransaction(effectTransaction);
-        let block = [];
-        if (effectTransaction) {
-            const effectReceivable = effectTransaction.receivables.filter(receivable => receivable.receivableZoopId === receivableId)[0];
-            setReceivable(effectReceivable);
-            if (effectReceivable) {
-                let feesFormatted = (effectReceivable.gross_amount && effectReceivable.amount) ?
-                    `- ${currencyFormat(parseFloat(`${round(parseFloat(effectReceivable.gross_amount) - parseFloat(effectReceivable.amount), 2)}`.replace(/[R$\.,]/g, '')))}` : '-';
-                block = [
-                    {
-                        header: 'Informações adicionais',
-                        body: [
-                            {
-                                title: 'Parcela',
-                                content: effectReceivable.installment
-                            },
-                            {
-                                title: 'Valor da parcela',
-                                content: `R$${parcelFormat(round(effectReceivable.gross_amount, 2))}`
-                            },
-                            {
-                                title: 'Tarifa Ziro Pay',
-                                content: feesFormatted
-                            },
-                            {
-                                title: 'Valor líquido',
-                                content: `R$${parcelFormat(round(effectReceivable.amount, 2))}`
-                            },
-                            {
-                                title: 'Recebimento',
-                                content: effectTransaction.receivement ? effectTransaction.receivement : 'D+30'
-                            },
-                            {
-                                title: 'Data recebimento',
-                                content: effectReceivable.paid_at ? dateFormat(effectReceivable.paid_at) : dateFormat(effectReceivable.expected_on)
-                            },
-                            {
-                                title: 'Status',
-                                content: effectReceivable.status === 'paid' ? 'Pago' : 'Pendente',
-                                color: matchStatusColor(effectReceivable.status)
-                            },
-                        ]
-                    }
-                ];
-            }
+  async function getTransaction(transactionId, setTransaction, setError, transaction) {
+    await fetch(transactionId, setTransaction, setError, transaction);
+  }
+
+  useEffect(() => {
+    //const transaction = transactions.filter(transaction => transaction.transactionId === transactionId)[0];
+    //setTransaction(transaction);
+    getTransaction(transactionId, setTransaction, setError, transaction).then(r => {
+      let block = [];
+      if (transaction) {
+        const effectReceivable = transaction.receivables.filter(receivable => receivable.receivableZoopId === receivableId)[0];
+        setReceivable(effectReceivable);
+        if (effectReceivable) {
+          let feesFormatted =
+            effectReceivable.gross_amount && effectReceivable.amount
+              ? `- ${currencyFormat(parseFloat(`${round(parseFloat(effectReceivable.gross_amount) - parseFloat(effectReceivable.amount), 2)}`.replace(/[R$\.,]/g, '')))}`
+              : '-';
+          block = [
+            {
+              header: 'Informações adicionais',
+              body: [
+                {
+                  title: 'Parcela',
+                  content: effectReceivable.installment,
+                },
+                {
+                  title: 'Valor da parcela',
+                  content: `R$${parcelFormat(round(effectReceivable.gross_amount, 2))}`,
+                },
+                {
+                  title: 'Tarifa Ziro Pay',
+                  content: feesFormatted,
+                },
+                {
+                  title: 'Valor líquido',
+                  content: `R$${parcelFormat(round(effectReceivable.amount, 2))}`,
+                },
+                {
+                  title: 'Recebimento',
+                  content: transaction.receivement ? transaction.receivement : 'D+30',
+                },
+                {
+                  title: 'Data recebimento',
+                  content: effectReceivable.paid_at ? dateFormat(effectReceivable.paid_at) : dateFormat(effectReceivable.expected_on),
+                },
+                {
+                  title: 'Status',
+                  content: effectReceivable.status === 'paid' ? 'Pago' : 'Pendente',
+                  color: matchStatusColor(effectReceivable.status),
+                },
+              ],
+            },
+          ];
         }
-        setBlocks(block);
-    }, []);
+      }
+      setBlocks(block);
+    });
+  }, [transaction]);
 
-    if (!transaction || !receivable) return <Error message='Lançamento inválido ou não encontrado, retorne e tente novamente.' type='noData' title='Erro ao buscar detalhes do lançamento' backRoute={`/transacoes/${transactionId}`} backRouteFunction={(route) => setLocation(route)} />;
-
+  if (!transaction || !receivable)
     return (
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} style={containerWithPadding}>
-            <Header type='icon-link' title='Detalhes do lançamento' navigateTo={`transacoes/${transactionId}`} icon='back' />
-            <div style={{ display: 'grid' }}>
-                <Details blocks={blocks} />
-            </div>
-        </motion.div>
+      <Error
+        message="Lançamento inválido ou não encontrado, retorne e tente novamente."
+        type="noData"
+        title="Erro ao buscar detalhes do lançamento"
+        backRoute={`/transacoes/${transactionId}`}
+        backRouteFunction={route => setLocation(route)}
+      />
     );
-}
 
-export default ReceivableDetails
+  return (
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} style={containerWithPadding}>
+      <Header type="icon-link" title="Detalhes do lançamento" navigateTo={`transacoes/${transactionId}`} icon="back" />
+      <div style={{ display: 'grid' }}>
+        <Details blocks={blocks} />
+      </div>
+    </motion.div>
+  );
+};
+
+export default ReceivableDetails;
--- src/App/Transactions/fetch.js
+++ src/App/Transactions/fetch.js
@@ -1,84 +1,76 @@
 import currencyFormat from '@ziro/currency-format';
 import { db } from '../../Firebase/index';
-import matchStatusColor from './matchStatusColor'
+import matchStatusColor from './matchStatusColor';
 import { dateFormat } from './utils';
 
 const fetch = (setIsLoading, setErrorLoading, payments, setPayments, zoopId, limit, lastDoc, setLastDoc, setTotalTransactions, setLoadingMore, docId, isCollaborator) => {
-    let query;
-    if (isCollaborator) {
-        query = db.collection('credit-card-payments')
-            .where('sellerZoopId', '==', zoopId)
-            .where('collaboratorId', '==', docId)
-            .orderBy('dateLinkCreated', 'desc')
-            .limit(limit);
-    } else {
-        query = db.collection('credit-card-payments')
-            .where('sellerZoopId', '==', zoopId)
-            .orderBy('dateLinkCreated', 'desc')
-            .limit(limit);
-    }
-    if (lastDoc) query = query.startAfter(lastDoc);
+  let query;
+  if (isCollaborator) {
+    query = db.collection('credit-card-payments').where('sellerZoopId', '==', zoopId).where('collaboratorId', '==', docId).orderBy('dateLinkCreated', 'desc').limit(limit);
+  } else {
+    query = db.collection('credit-card-payments').where('sellerZoopId', '==', zoopId).orderBy('dateLinkCreated', 'desc').limit(limit);
+  }
+  if (lastDoc) query = query.startAfter(lastDoc);
 
-    const run = async () => {
-        try {
-            await query.onSnapshot(async snapshot => {
-                let collectionData;
-                if (isCollaborator) {
-                    collectionData = await db.collection('credit-card-payments')
-                        .where('sellerZoopId', '==', zoopId)
-                        .where('collaboratorId', '==', docId)
-                        .get();
-                } else {
-                    collectionData = await db.collection('credit-card-payments').where('sellerZoopId', '==', zoopId).get();
-                }
-                setTotalTransactions(collectionData.docs.length);
-                const paymentDoc = [];
-                if (!snapshot.empty) {
-                    snapshot.forEach(doc => {
-                        const { charge, date, fees, installments, dateLinkCreated, transactionZoopId,
-                            maxInstallments, sellerZoopId, status, buyerRazao, receivables, receivement, receiptId } = doc.data();
-                        const chargeFormatted = currencyFormat(charge);
-                        const dateFormatted = date ? dateFormat(date) : '';
-                        paymentDoc.push({
-                            transactionZoopId: transactionZoopId ? transactionZoopId : '',
-                            transactionId: doc.id,
-                            charge: chargeFormatted,
-                            dateLinkCreated,
-                            date: dateFormatted,
-                            fees: fees ? fees : '',
-                            installments: installments ? installments : '',
-                            maxInstallments: maxInstallments ? maxInstallments : '',
-                            seller: buyerRazao ? buyerRazao : '-',
-                            sellerZoopId: sellerZoopId ? sellerZoopId : '',
-                            status: status ? status : '',
-                            statusColor: matchStatusColor(status),
-                            buyerRazao,
-                            receivables: receivables ? receivables : [],
-                            receivement,
-                            receiptId: receiptId ? receiptId : '',
-                        });
-                    });
-                    setLastDoc(snapshot.docs[snapshot.docs.length - 1]);
-                    setPayments([...payments, ...paymentDoc]);
-                } else {
-                    setLastDoc(null);
-                    setPayments([]);
-                }
-                setIsLoading(false);
-                setLoadingMore(false);
-            }, error => {
-                console.log(error);
-                setIsLoading(false);
-                setLoadingMore(false);
+  const run = async () => {
+    try {
+      await query.onSnapshot(
+        async snapshot => {
+          let collectionData;
+          if (isCollaborator) {
+            collectionData = await db.collection('credit-card-payments').where('sellerZoopId', '==', zoopId).where('collaboratorId', '==', docId).get();
+          } else {
+            collectionData = await db.collection('credit-card-payments').where('sellerZoopId', '==', zoopId).get();
+          }
+          setTotalTransactions(collectionData.docs.length);
+          const paymentDoc = [];
+          if (!snapshot.empty) {
+            snapshot.forEach(doc => {
+              const { charge, date, fees, installments, dateLinkCreated, transactionZoopId, maxInstallments, sellerZoopId, status, buyerRazao, receivables, receivement, receiptId } = doc.data();
+              const chargeFormatted = currencyFormat(charge);
+              const dateFormatted = date ? dateFormat(date) : '';
+              paymentDoc.push({
+                transactionZoopId: transactionZoopId ? transactionZoopId : '',
+                transactionId: doc.id,
+                charge: chargeFormatted,
+                dateLinkCreated,
+                date: dateFormatted,
+                fees: fees ? fees : '',
+                installments: installments ? installments : '',
+                maxInstallments: maxInstallments ? maxInstallments : '',
+                seller: buyerRazao ? buyerRazao : '-',
+                sellerZoopId: sellerZoopId ? sellerZoopId : '',
+                status: status ? status : '',
+                statusColor: matchStatusColor(status),
+                buyerRazao,
+                receivables: receivables ? receivables : [],
+                receivement,
+                receiptId: receiptId ? receiptId : '',
+              });
             });
-        } catch (error) {
-            console.log(error);
-            setErrorLoading(true);
-            setIsLoading(false);
-            setLoadingMore(false);
-        }
+            setLastDoc(snapshot.docs[snapshot.docs.length - 1]);
+            setPayments([...payments, ...paymentDoc]);
+          } else {
+            setLastDoc(null);
+            setPayments([]);
+          }
+          setIsLoading(false);
+          setLoadingMore(false);
+        },
+        error => {
+          console.log(error);
+          setIsLoading(false);
+          setLoadingMore(false);
+        },
+      );
+    } catch (error) {
+      console.log(error);
+      setErrorLoading(true);
+      setIsLoading(false);
+      setLoadingMore(false);
     }
-    run();
+  };
+  run();
 };
 
 export default fetch;
