97e82bb44cdeaf7c5120fee0d66294d31b7056a5
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-07-01 12:36:12 -0300

    change to realtime fetch

---
+++ src/App/GerarBoleto/fetchPending.js
@@ -0,0 +1,98 @@
+import axios from 'axios'
+import currencyFormat from '@ziro/currency-format'
+import { db } from '../../Firebase/index'
+import matchStatusColor from './utils/matchStatusColor'
+
+const fetchPending = (state) => {
+    const {setIsError, seller, setPendingBoletos, setPendingDuplicatas, setFinishPending } = state
+    const queryPending = db.collection('pending-commission').where('fantasia', '==', seller)
+    const source = axios.CancelToken.source()
+    const run = async () => {
+        let pendingDuplicatas = []
+        let pendingBoletos = []
+        try {
+            queryPending.onSnapshot(
+                async snapPending => {
+                    if (!snapPending.empty) {
+                        snapPending.forEach((doc) => {
+                            const { pending_polos } = doc.data()
+                            if(pending_polos){
+                                pending_polos.map((polo) => {
+                                 const arrayReceitas = polo.billets.map((item) => {
+                                     if(typeof item.receita === 'number'){
+                                         return item.receita
+                                     }
+                                         return Number(item.receita.replace('.','').replace(',','.'))
+                                     
+                                 })
+                                 const arrayPolo = polo.billets[0].rua.split(' ')
+                                 const enderecoSimple = `${arrayPolo[0].replace(',','')}, ${arrayPolo[arrayPolo.length-1]}`
+                                 const totalReceitas = arrayReceitas.reduce((a,b) => a+b)
+                                    if(polo.status === 'Comissões em Aberto'){
+                                        pendingDuplicatas.push({
+                                            id:polo.transactionZoopId,
+                                            charge: currencyFormat(Math.round(totalReceitas*100)),
+                                            date: '-',
+                                            seller:`${enderecoSimple}`,
+                                            status:polo.status,
+                                            statusColor: matchStatusColor(polo.status)
+                                        })
+                                        pendingBoletos.push({
+                                            contador: `${polo.counter}`,
+                                            id:polo.transactionZoopId,
+                                            fabricante:polo.fantasia,
+                                            status:polo.status,
+                                            values:polo.billets,
+                                            relatorio: `Relatório ${enderecoSimple}`,
+                                            endereco: `${polo.billets[0].polo} - ${polo.billets[0].rua}`
+                                        })
+                                    }
+                                })
+                            }else{
+                                const arrayReceitas = doc.data().billets.map((item) => {
+                                if(typeof item.receita === 'number'){
+                                    return item.receita
+                                }
+                                    return Number(item.receita.replace('.','').replace(',','.'))
+                                
+                            })
+                            const arrayPolo = doc.data().billets[0].rua.split(' ')
+                            const enderecoSimple = `${arrayPolo[0].replace(',','')}, ${arrayPolo[arrayPolo.length-1]}`
+                            const totalReceitas = arrayReceitas.reduce((a,b) => a+b)
+                            if(doc.data().status === 'Comissões em Aberto'){
+                            pendingDuplicatas.push({
+                                id:'relatorio_futuro',
+                                charge: currencyFormat(Math.round(totalReceitas*100)),
+                                date: '-',
+                                seller:`${enderecoSimple}`,
+                                status:doc.data().status,
+                                statusColor: matchStatusColor(doc.data().status)
+                            })
+                            pendingBoletos.push({
+                                contador: 'Futuro',
+                                id:'relatorio_futuro',
+                                fabricante:doc.data().fantasia,
+                                status:doc.data().status,
+                                values:doc.data().billets,
+                                relatorio: `Relatório ${enderecoSimple}`,
+                                endereco: `${doc.data().billets[0].polo} - ${doc.data().billets[0].rua}`
+                            })    
+                            }
+                            }
+                        })
+                    }
+                    setPendingDuplicatas(pendingDuplicatas)
+                    setPendingBoletos(pendingBoletos)
+                })
+                setFinishPending(true)
+        } catch (error) {
+            if (error.response) console.log(error.response)
+            else console.log(error)
+            setIsError(true)
+        }
+    }
+    run()
+    return () => source.cancel('Canceled fetch request. Component unmounted')
+}
+
+export default fetchPending
--- src/App/GerarBoleto/index.js
+++ src/App/GerarBoleto/index.js
@@ -1,25 +1,35 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { motion } from 'framer-motion'
 import Spinner from '@bit/vitorbarbosa19.ziro.spinner'
-import { spinner } from './styles';
 import Error from '@bit/vitorbarbosa19.ziro.error'
+import { spinner } from './styles'
 import { userContext } from '../appContext'
 import DuplicataDetails from './DuplicataDetails/index'
 import RelatorioList from './RelatorioList/index'
-import fetch from './fetch'
+import fetchPayments from './fetchPayments'
+import fetchPending from './fetchPending'
+import fetchJoin from './fetchJoin'
 
 const GerarBoleto = ({ boletbankId, boletId }) => {
     const { zoopId, docId, razao, email, fantasy } = useContext(userContext)
-    const [firstTicket, setfisrtTicket] = useState([]);
-    const [ticket, setTicket] = useState([]);
     const [isError, setIsError] = useState(false)
     const [isLoading, setIsLoading] = useState(true);
     const [charge, setCharge] = useState('')
     const [url, setUrl] = useState('')
     const [urlLoad, setUrlLoad] = useState(false)
     const [maxInstallments, setMaxInstallments] = useState('')
-    const state = { seller: fantasy, sellerId: zoopId, razao, charge, maxInstallments, docId, url, urlLoad, email, setCharge, setMaxInstallments, setIsError, setIsLoading, setUrl, setUrlLoad, setfisrtTicket, setTicket }
-    useEffect(() => fetch(state), []);
+    const [paymentDuplicatas, setPaymentDuplicatas] = useState([])
+    const [paymentBoletos, setPaymentBoletos] = useState([])
+    const [pendingBoletos, setPendingBoletos] = useState([])
+    const [pendingDuplicatas, setPendingDuplicatas] = useState([])
+    const [finishPayments, setFinishPayments] = useState(false)
+    const [finishPending, setFinishPending] = useState(false)
+    const [ticket, setTicket] = useState([])
+    const [firstTicket, setfisrtTicket] = useState([])
+    const state = { finishPayments, setFinishPayments, finishPending, setFinishPending, paymentDuplicatas, setPaymentDuplicatas, paymentBoletos, setPaymentBoletos,pendingBoletos, setPendingBoletos,pendingDuplicatas, setPendingDuplicatas, seller:fantasy, sellerId: zoopId, razao, charge, maxInstallments, docId, url, urlLoad, email, setCharge, setMaxInstallments, setIsError, setIsLoading, setUrl, setUrlLoad, setfisrtTicket, setTicket }
+    useEffect(() => fetchPayments(state), [])
+    useEffect(() => fetchPending(state), [])
+    useEffect(() => fetchJoin(state), [paymentDuplicatas, paymentBoletos, pendingBoletos, pendingDuplicatas])
     if (isLoading)
         return (
             <div style={spinner}>
@@ -28,7 +38,7 @@ const GerarBoleto = ({ boletbankId, boletId }) => {
         );
     if (isError) return <Error />
     if (boletbankId && boletId) return <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}><DuplicataDetails transactions={firstTicket} boletbankId={boletbankId} boletId={boletId} sellerId={state.sellerId} /></motion.div>
-    if (boletbankId) return <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}><DuplicataDetails transactions={firstTicket} boletbankId={boletbankId} boletId={''} sellerId={state.sellerId} /></motion.div>;
+    if (boletbankId) return <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}><DuplicataDetails transactions={firstTicket} boletbankId={boletbankId} boletId="" sellerId={state.sellerId} /></motion.div>;
     return <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}><RelatorioList transactions={ticket} /></motion.div>;
 }
 
