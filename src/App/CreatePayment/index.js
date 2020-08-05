59f8057c666d2da46af2ded37f71262162cd76fd
Alessandro <vyseclown@gmail.com>
2020-08-05 13:19:11 -0300

    Minor change to create payment

--- src/App/CreatePayment/index.js
+++ src/App/CreatePayment/index.js
@@ -9,67 +9,58 @@ import InputMoney from '@bit/vitorbarbosa19.ziro.input-money';
 import { userContext } from '../appContext';
 
 const CreatePayment = () => {
-    const { fantasy, zoopId, docId, role, fname, brand } = useContext(userContext);
-    const [charge, setCharge] = useState('');
-    const [maxInstallments, setMaxInstallments] = useState('');
-    const [observations, setObservations] = useState('');
-    const state = {
-        seller: capitalize(fantasy),
-        sellerId: zoopId,
-        charge,
-        maxInstallments,
-        isCollaborator: role !== '',
-        docId,
-        fname,
-        brand,
-        setCharge,
-        setMaxInstallments,
-        observations,
-        setObservations,
-    };
+  const { fantasy, zoopId, docId, role, fname, brand } = useContext(userContext);
+  const [charge, setCharge] = useState('');
+  const [maxInstallments, setMaxInstallments] = useState('');
+  const [observations, setObservations] = useState('');
+  const state = {
+    seller: capitalize(fantasy),
+    sellerId: zoopId,
+    charge,
+    maxInstallments,
+    isCollaborator: role !== '',
+    docId,
+    fname,
+    brand,
+    setCharge,
+    setMaxInstallments,
+    observations,
+    setObservations,
+  };
 
-    const validations = [
-        {
-            name: 'charge',
-            validation: value => value > 9 && value <= 3000000,
-            value: charge,
-            message: 'Deve ser entre 0,10 e 30mil',
-        },
-        {
-            name: 'maxInstallments',
-            validation: value => (brand ? parseInt(value) > 0 && parseInt(value) < 5 : parseInt(value) > 0 && parseInt(value) <= 10),
-            value: maxInstallments,
-            message: brand ? 'Deve ser entre 1 e 4' : 'Deve ser entre 1 e 10',
-        },
-    ];
-    return (
-        <Form
-            validations={validations}
-            sendToBackend={sendToBackend ? sendToBackend(state) : () => null}
-            inputs={[
-                <FormInput
-                    name="charge"
-                    label="Valor a cobrar"
-                    input={
-                        <InputMoney
-                            value={charge}
-                            setValue={setCharge}
-                        />
-                    }
-                />,
-                <FormInput
-                    name="maxInstallments"
-                    label="Parcelamento máximo"
-                    input={<InputText value={maxInstallments} onChange={({ target: { value } }) => setMaxInstallments(maskInput(value, '##', true))} placeholder="10" />}
-                />,
-                <FormInput
-                    name="observation"
-                    label="Observações (opcional)"
-                    input={<InputText value={observations} onChange={({ target: { value } }) => setObservations(value)} placeholder="Romaneio, nome do cliente, etc" />}
-                />,
-            ]}
-        />
-    );
+  const validations = [
+    {
+      name: 'charge',
+      validation: value => value > 9 && value <= 3000000,
+      value: charge,
+      message: 'Deve ser entre 0,10 e 30mil',
+    },
+    {
+      name: 'maxInstallments',
+      validation: value => (fantasy === 'ZIRO' ? parseInt(value) > 0 && parseInt(value) < 5 : parseInt(value) > 0 && parseInt(value) <= 10),
+      value: maxInstallments,
+      message: fantasy === 'ZIRO' ? 'Deve ser entre 1 e 4' : 'Deve ser entre 1 e 10',
+    },
+  ];
+  return (
+    <Form
+      validations={validations}
+      sendToBackend={sendToBackend ? sendToBackend(state) : () => null}
+      inputs={[
+        <FormInput name="charge" label="Valor a cobrar" input={<InputMoney value={charge} setValue={setCharge} />} />,
+        <FormInput
+          name="maxInstallments"
+          label="Parcelamento máximo"
+          input={<InputText value={maxInstallments} onChange={({ target: { value } }) => setMaxInstallments(maskInput(value, '##', true))} placeholder="10" />}
+        />,
+        <FormInput
+          name="observation"
+          label="Observações (opcional)"
+          input={<InputText value={observations} onChange={({ target: { value } }) => setObservations(value)} placeholder="Romaneio, nome do cliente, etc" />}
+        />,
+      ]}
+    />
+  );
 };
 
 export default CreatePayment;
