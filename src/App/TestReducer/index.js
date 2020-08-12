7a492748f2a2dd14d20531e77516bb269133339a
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-08-12 12:10:38 -0300

    add payload param

--- src/App/TestReducer/index.js
+++ src/App/TestReducer/index.js
@@ -8,33 +8,37 @@ import { db } from '../../Firebase/index'
 const reducer = (state, action) => {
     const machine = {
         changeToBatata: async () => {
-            console.log({...state, cpf:'batata'})
+            console.log({...state,...action.payload, cpf:'batata'})
             try {
                 await db.collection('suppliers').doc('bYg5rhaI73cSqg3wvglSOmDXH5d2').update({cpf:'batata'}) 
+                return {...state, ...action.payload, cpf:'batata'}
             } catch (error) {
                 console.log(error)
             }
         },
         changeTo000: async () => {
-            console.log({...state, cpf:'000.000.000-00'})
+            console.log({...state,...action.payload, cpf:'000.000.000-00'})
             try {
                 await db.collection('suppliers').doc('bYg5rhaI73cSqg3wvglSOmDXH5d2').update({cpf:'000.000.000-00'})
+                return {...state, ...action.payload, cpf:'000.000.000-00'}
             } catch (error) {
                 console.log(error)
             }
         },
         changeToSP: async () => {
-            console.log({...state, cpf:'000.000.000-00'})
+            console.log({...state,...action.payload, estado:'SP'})
             try {
                 await db.collection('suppliers').doc('bYg5rhaI73cSqg3wvglSOmDXH5d2').update({estado:'SP'})
+                return {...state, estado:'SP'}
             } catch (error) {
                 console.log(error)
             }
         },
         changeToRJ: async () => {
-            console.log({...state, cpf:'000.000.000-00'})
+            console.log({...state,...action.payload, estado:'RJ'})
             try {
                 await db.collection('suppliers').doc('bYg5rhaI73cSqg3wvglSOmDXH5d2').update({estado:'RJ'})
+                return {...state, estado:'RJ'}
             } catch (error) {
                 console.log(error)
             }
@@ -46,11 +50,10 @@ const reducer = (state, action) => {
 const TestReducer = () => {
     const [isError, setIsError] = useState(false)
     const [isLoading, setIsLoading] = useState(true);
-    const [supplier, setSupplier] = useState('')
+    const [supplier, setSupplier] = useState({})
     const state = {setIsError, setIsLoading, setSupplier}
     useEffect(() => fetch(state), [])
-    const [supplierObj, dispatch] = useReducer(reducer, supplier)
-    console.log(supplierObj)
+    const [supplierObj, dispatch] = useReducer(reducer, {})
     if (isLoading)
         return (
             <div style={spinner}>
@@ -61,20 +64,15 @@ const TestReducer = () => {
     return (
         <div>
             <div style={{display:'flex', justifyContent:'space-around'}}>
-                <h2>cpf: {supplier.cpf}</h2>
-                <button onClick={() => dispatch({type: 'changeToBatata', payload:supplier})}>Change to batata</button>
-                <button onClick={() => dispatch({type: 'changeTo000', payload:supplier})}>Change to 000.000.000-00</button>
+                <h2>cpf: {supplier.cpf || supplierObj.cpf}</h2>
+                <button type='submit' onClick={() => dispatch({type: 'changeToBatata', payload:supplier})}>Change to batata</button>
+                <button type='submit' onClick={() => dispatch({type: 'changeTo000', payload:supplier})}>Change to 000.000.000-00</button>
             </div>
             <div style={{display:'flex', justifyContent:'space-around'}}>
                 <h2>estado: {supplier.estado}</h2>
-                <button onClick={() => dispatch({type: 'changeToRJ'})}>Change to RJ</button>
-                <button onClick={() => dispatch({type: 'changeToSP'})}>Change to SP</button>
+                <button type='submit'  onClick={() => dispatch({type: 'changeToRJ'})}>Change to RJ</button>
+                <button type='submit'  onClick={() => dispatch({type: 'changeToSP'})}>Change to SP</button>
             </div>
-            <h2>nascimento: {supplier.nascimento}</h2>
-            <button>Change to batata</button> 
-            <h2>telefone: {supplier.telefone}</h2>
-            <button>Change to batata</button> 
-            <h2>bairro: {supplier.bairro}</h2> 
         </div>
     );
 }
