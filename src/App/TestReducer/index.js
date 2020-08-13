005ed4e5722485e83050fec26a7c2ea1dc621c0e
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-08-13 00:22:54 -0300

    fix style

--- src/App/TestReducer/index.js
+++ src/App/TestReducer/index.js
@@ -1,51 +1,9 @@
 import React, { useState, useReducer, useEffect } from 'react'
 import Spinner from '@bit/vitorbarbosa19.ziro.spinner'
 import Error from '@bit/vitorbarbosa19.ziro.error'
-import { spinner } from './styles'
+import { spinner, btnStyle, titleStyle, subTitleStyle, line } from './styles'
+import reducer from './customReducer'
 import fetch from './fetch'
-import { db } from '../../Firebase/index'
-
-const reducer = (state, action) => {
-    const machine = {
-        changeToBatata: async () => {
-            console.log({...state,...action.payload, cpf:'batata'})
-            try {
-                await db.collection('suppliers').doc('bYg5rhaI73cSqg3wvglSOmDXH5d2').update({cpf:'batata'}) 
-                return {...state, ...action.payload, cpf:'batata'}
-            } catch (error) {
-                console.log(error)
-            }
-        },
-        changeTo000: async () => {
-            console.log({...state,...action.payload, cpf:'000.000.000-00'})
-            try {
-                await db.collection('suppliers').doc('bYg5rhaI73cSqg3wvglSOmDXH5d2').update({cpf:'000.000.000-00'})
-                return {...state, ...action.payload, cpf:'000.000.000-00'}
-            } catch (error) {
-                console.log(error)
-            }
-        },
-        changeToSP: async () => {
-            console.log({...state,...action.payload, estado:'SP'})
-            try {
-                await db.collection('suppliers').doc('bYg5rhaI73cSqg3wvglSOmDXH5d2').update({estado:'SP'})
-                return {...state, estado:'SP'}
-            } catch (error) {
-                console.log(error)
-            }
-        },
-        changeToRJ: async () => {
-            console.log({...state,...action.payload, estado:'RJ'})
-            try {
-                await db.collection('suppliers').doc('bYg5rhaI73cSqg3wvglSOmDXH5d2').update({estado:'RJ'})
-                return {...state, estado:'RJ'}
-            } catch (error) {
-                console.log(error)
-            }
-        },
-    }
-        machine[action.type]()
-    }
 
 const TestReducer = () => {
     const [isError, setIsError] = useState(false)
@@ -71,7 +29,7 @@ const TestReducer = () => {
                 <h2 style={subTitleStyle}>updateFirebase</h2>
                 <button type="submit" style={btnStyle} onClick={() => dispatch({type:'updateFirebase', payload:{doc:links.id,update:{status:'Aguardando Teste'}}})}>Update Firebase</button>
             </div>
-        <div>
+            <div>
                 <h2 style={subTitleStyle}>sendMsg</h2>
                 <button type="submit" style={btnStyle} onClick={() => dispatch({type:'sendWhats', payload:{celular:11995643901,msg:'Olá Ahmad'}})}>Send Whats</button>
             </div>
--- src/App/TestReducer/styles.js
+++ src/App/TestReducer/styles.js
@@ -1,6 +1,39 @@
 export const
-
     spinner = {
         display: 'grid',
         justifyItems: 'center'
+    },
+    btnStyle = {
+        width:'90%',
+        maxWidth:'500px',
+        padding:'10px',
+        backgroundColor:'#242424',
+        fontWeight:'bold',
+        borderRadius:'200px',
+        cursor: 'pointer',
+        border: 'none',
+        color:'white',
+        margin:'0, auto',
+        marginBottom:'20px',
+        boxShadow: '0px 5px 15px -4px rgba(34,34,34,0.45)',
+        marginLeft:'20px'
+    },
+    titleStyle ={
+        fontFamily:'Rubik',
+        fontSize:'2rem',
+        fontWeigth:'600px',
+        textAlign:'center',
+        marginBottom:'0'
+    },
+    subTitleStyle ={
+        fontFamily:'Rubik',
+        fontSize:'2rem',
+        fontWeigth:'600px'
+    },
+    line ={
+        height:'4px',
+        width:'80px',
+        margin:'0 auto',
+        marginTop:'5px',
+        backgroundColor:'yellow',
     }
