777805e58c1be6adc3a3089ebf02581c3ecdbe19
Alessandro <vyseclown@gmail.com>
2020-10-07 19:42:33 -0300

    Changing the name of the button

--- src/App/CreatePayment/index.js
+++ src/App/CreatePayment/index.js
@@ -86,6 +86,7 @@ const CreatePayment = () => {
   return hasSellerZoopPlan && (hasSellerZoopPlan.antiFraud.amount || hasSellerZoopPlan.antiFraud.percentage) ? (
     insurance === null || insurance ? (
       <Form
+        buttonName="Criar Link"
         validations={validations}
         sendToBackend={sendToBackend ? sendToBackend(state) : () => null}
         inputs={[
@@ -154,6 +155,7 @@ const CreatePayment = () => {
       />
     ) : (
       <Form
+        buttonName="Criar Link"
         validations={validations}
         sendToBackend={sendToBackend ? sendToBackend(state) : () => null}
         inputs={[
@@ -245,6 +247,7 @@ const CreatePayment = () => {
     )
   ) : (
     <Form
+      buttonName="Criar Link"
       validations={validations}
       sendToBackend={sendToBackend ? sendToBackend(state) : () => null}
       inputs={[
