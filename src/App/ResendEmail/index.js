1a738d16df8bd5b8b545a77dbc15eaa2c8572d74
Antonio Decanini <antonio.decanini@hotmail.com>
2020-11-23 10:27:36 -0300

    Fix previous route in resetPass and resendEmail

--- src/App/ResendEmail/index.js
+++ src/App/ResendEmail/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ResendEmail from '@bit/vitorbarbosa19.ziro.resend-email'
 import sendToBackend from './sendToBackend'
 
-export default () => <ResendEmail sendToBackend={sendToBackend} navigateTo='/login' />
+export default () => <ResendEmail sendToBackend={sendToBackend} navigateTo='/problemas-acesso' />
--- src/App/ResetPass/index.js
+++ src/App/ResetPass/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ResetPass from '@bit/vitorbarbosa19.ziro.reset-pass'
 import sendToBackend from './sendToBackend'
 
-export default () => <ResetPass sendToBackend={sendToBackend} navigateTo='/login' />
+export default () => <ResetPass sendToBackend={sendToBackend} navigateTo='/problemas-acesso' />
