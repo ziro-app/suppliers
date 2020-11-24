20babc964207dc3b1f022f7c3392fdb9b9084e49
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-11-24 16:32:11 -0300

    Payment link fix

--- src/App/CreatePayment/sendToBackend.js
+++ src/App/CreatePayment/sendToBackend.js
@@ -3,109 +3,109 @@ import linkMessage from './utils/linkMessage';
 import currencyFormat from '@ziro/currency-format';
 
 const checkCollaborator = async docId => {
-  const collaborator = await db.collection('collaborators').doc(docId).get();
-  return collaborator.exists;
+    const collaborator = await db.collection('collaborators').doc(docId).get();
+    return collaborator.exists;
 };
 
 const sendToBackend = state => () => {
-  const {
-    seller,
-    sellerId,
-    charge,
-    installmentsMax,
-    isCollaborator,
-    docId,
-    fname,
-    brand,
-    setCharge,
-    setInstallmentsMax,
-    observations,
-    setObservations,
-    insurance,
-    setInsurance,
-    setInsurenceDropdownValue,
-    hasSellerZoopPlan,
-    checkoutWithoutRegister,
-    setCheckoutWithoutRegister,
-  } = state;
-  const baseUrl = process.env.HOMOLOG ? 'http://localhost:8080/pagamento/' : 'https://ziro.app/pagamento/';
+    const {
+        seller,
+        sellerId,
+        charge,
+        installmentsMax,
+        isCollaborator,
+        docId,
+        fname,
+        brand,
+        setCharge,
+        setInstallmentsMax,
+        observations,
+        setObservations,
+        insurance,
+        setInsurance,
+        setInsurenceDropdownValue,
+        hasSellerZoopPlan,
+        checkoutWithoutRegister,
+        setCheckoutWithoutRegister,
+    } = state;
+    const baseUrl = process.env.HOMOLOG ? 'http://localhost:8080/pagamento/' : 'https://ziro.app/pagamento/';
 
-  return new Promise(async (resolve, reject) => {
-    try {
-      const nowDate = fs.FieldValue.serverTimestamp();
-      if (seller && sellerId) {
-        let docRef;
-        if (isCollaborator) {
-          const isValid = await checkCollaborator(docId);
-          if (isValid) {
-            docRef = await db.collection('credit-card-payments').add({
-              dateLinkCreated: nowDate,
-              dateLastUpdate: nowDate,
-              seller,
-              sellerZoopId: sellerId,
-              charge,
-              installmentsMax: `${parseInt(installmentsMax)}`,
-              status: 'Aguardando Pagamento',
-              collaboratorId: docId,
-              collaboratorName: fname,
-              //onBehalfOfBrand: brand ? brand : seller,
-              observations,
-              insurance: insurance !== null ? insurance : true,
-              isNewPlan: false,
-              sellerZoopPlan: hasSellerZoopPlan || null,
-              checkoutWithoutRegister: checkoutWithoutRegister || false,
-            });
-          } else throw { msg: 'Permissão insuficiente', customError: true };
-        } else {
-          docRef = await db.collection('credit-card-payments').add({
-            dateLinkCreated: nowDate,
-            dateLastUpdate: nowDate,
-            seller,
-            sellerZoopId: sellerId,
-            charge,
-            installmentsMax: `${parseInt(installmentsMax)}`,
-            status: 'Aguardando Pagamento',
-            observations,
-            insurance: insurance !== null ? insurance : true,
-            sellerZoopPlan: hasSellerZoopPlan || null,
-            isNewPlan: false,
-            checkoutWithoutRegister: checkoutWithoutRegister || false,
-          });
-        }
+    return new Promise(async (resolve, reject) => {
         try {
-          const doc = await docRef.get();
-          const formattedCharge = currencyFormat(charge);
-          if (doc) await navigator.clipboard.writeText(linkMessage(baseUrl, doc.id, seller, formattedCharge, installmentsMax, checkoutWithoutRegister));
+            const nowDate = fs.FieldValue.serverTimestamp();
+            if (seller && sellerId) {
+                let docRef;
+                if (isCollaborator) {
+                    const isValid = await checkCollaborator(docId);
+                    if (isValid) {
+                        docRef = await db.collection('credit-card-payments').add({
+                            dateLinkCreated: nowDate,
+                            dateLastUpdate: nowDate,
+                            seller,
+                            sellerZoopId: sellerId,
+                            charge,
+                            installmentsMax: `${parseInt(installmentsMax)}`,
+                            status: 'Aguardando Pagamento',
+                            collaboratorId: docId,
+                            collaboratorName: fname,
+                            //onBehalfOfBrand: brand ? brand : seller,
+                            observations,
+                            insurance: insurance !== null ? insurance : true,
+                            isNewPlan: false,
+                            sellerZoopPlan: hasSellerZoopPlan || null,
+                            checkoutWithoutRegister: checkoutWithoutRegister || false,
+                        });
+                    } else throw { msg: 'Permissão insuficiente', customError: true };
+                } else {
+                    docRef = await db.collection('credit-card-payments').add({
+                        dateLinkCreated: nowDate,
+                        dateLastUpdate: nowDate,
+                        seller,
+                        sellerZoopId: sellerId,
+                        charge,
+                        installmentsMax: `${parseInt(installmentsMax)}`,
+                        status: 'Aguardando Pagamento',
+                        observations,
+                        insurance: insurance !== null ? insurance : true,
+                        sellerZoopPlan: hasSellerZoopPlan || null,
+                        isNewPlan: false,
+                        checkoutWithoutRegister: checkoutWithoutRegister || false,
+                    });
+                }
+                try {
+                    const doc = await docRef.get();
+                    const formattedCharge = currencyFormat(charge);
+                    if (doc) await navigator.clipboard.writeText(linkMessage(baseUrl, doc.id, seller, formattedCharge, installmentsMax));
+                } catch (error) {
+                    throw { msg: 'Erro ao realizar a cópia', copyError: true };
+                }
+                resolve('Link copiado');
+                setCharge('');
+                setInstallmentsMax('');
+                setObservations('');
+                setInsurance(null);
+                setInsurenceDropdownValue('');
+                setCheckoutWithoutRegister(false);
+            } else {
+                throw { msg: 'Vendedor não encontrado', customError: true };
+            }
         } catch (error) {
-          throw { msg: 'Erro ao realizar a cópia', copyError: true };
+            if (error.copyError) {
+                resolve('Link criado. Acesse na aba de Vendas');
+                setCharge('');
+                setInstallmentsMax('');
+                setInsurance(null);
+                setInsurenceDropdownValue('');
+                setCheckoutWithoutRegister(false);
+            }
+            if (error.customError) reject(error);
+            else {
+                console.log(error);
+                if (error.response) console.log(error.response);
+                reject('Erro ao criar cobrança');
+            }
         }
-        resolve('Link copiado');
-        setCharge('');
-        setInstallmentsMax('');
-        setObservations('');
-        setInsurance(null);
-        setInsurenceDropdownValue('');
-        setCheckoutWithoutRegister(false);
-      } else {
-        throw { msg: 'Vendedor não encontrado', customError: true };
-      }
-    } catch (error) {
-      if (error.copyError) {
-        resolve('Link criado. Acesse na aba de Vendas');
-        setCharge('');
-        setInstallmentsMax('');
-        setInsurance(null);
-        setInsurenceDropdownValue('');
-        setCheckoutWithoutRegister(false);
-      }
-      if (error.customError) reject(error);
-      else {
-        console.log(error);
-        if (error.response) console.log(error.response);
-        reject('Erro ao criar cobrança');
-      }
-    }
-  });
+    });
 };
 
 export default sendToBackend;
--- src/App/CreatePayment/utils/linkMessage.js
+++ src/App/CreatePayment/utils/linkMessage.js
@@ -1,22 +1,22 @@
-const linkMessage = (baseUrl, docId, seller, charge, installmentsMax, checkoutWithoutRegister) => {
-  const link = checkoutWithoutRegister ? `${baseUrl}${docId}/resumo?doc&cwr` : `${baseUrl}${docId}/resumo?doc`;
-  let message = ``;
+const linkMessage = (baseUrl, docId, seller, charge, installmentsMax) => {
+    const link = `${baseUrl}${docId}`;
+    let message = ``;
 
-  if (seller && charge && installmentsMax) {
-      message = `Pague suas compras na *${seller}* no valor de *${charge}* em até *${installmentsMax}x s/ juros*. Só clicar 😎\n${link}`;
-  } else if (seller && charge) {
-      message = `Pague suas compras na *${seller}* no valor de *${charge}*. Só clicar 😎\n${link}`;
-  } else if (charge && installmentsMax) {
-      message = `Pague sua compra no valor de *${charge}* em até *${installmentsMax}x s/ juros*. Só clicar 😎\n${link}`;
-  } else if (seller || seller && installmentsMax) {
-      message = `Pague sua compra na *${seller}*. Só clicar 😎\n${link}`;
-  } else if (charge) {
-      message = `Pague sua compra no valor de *${charge}*. Só clicar 😎\n${link}`;
-  } else {
-      message = `Pague sua compra. Só clicar 😎\n${link}`;
-  }
+    if (seller && charge && installmentsMax) {
+        message = `Pague suas compras na *${seller}* no valor de *${charge}* em até *${installmentsMax}x s/ juros*. Só clicar 😎\n${link}`;
+    } else if (seller && charge) {
+        message = `Pague suas compras na *${seller}* no valor de *${charge}*. Só clicar 😎\n${link}`;
+    } else if (charge && installmentsMax) {
+        message = `Pague sua compra no valor de *${charge}* em até *${installmentsMax}x s/ juros*. Só clicar 😎\n${link}`;
+    } else if (seller || seller && installmentsMax) {
+        message = `Pague sua compra na *${seller}*. Só clicar 😎\n${link}`;
+    } else if (charge) {
+        message = `Pague sua compra no valor de *${charge}*. Só clicar 😎\n${link}`;
+    } else {
+        message = `Pague sua compra. Só clicar 😎\n${link}`;
+    }
 
-  return message;
+    return message;
 };
 
 export default linkMessage;
