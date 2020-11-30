833a25d348107cbe9c79323a50dc76539b6bcc8f
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-11-30 15:32:25 -0300

    feat: add filter selectors

--- src/App/Transactions/styles.js
+++ src/App/Transactions/styles.js
@@ -1,6 +1,26 @@
-export const
+export const container = { 
+    display: 'grid',
+    gridGap: '8px',
+    marginBottom: '25px'
+}
 
-    spinner = {
-        display: 'grid',
-        justifyItems: 'center'
-    }
+export const containerClearAll = {
+    display:'flex',
+    justifyContent:"flex-end"
+}
+
+export const btnClearAll = {
+    display:'flex',
+    justifyContent:"flex-end",
+    cursor:'pointer',
+    outline:'none',
+    border:'none',
+    background:'none',
+    textAlign:'rigth'
+}
+
+export const textClearAll = {
+    fontSize: '1.2rem',
+    fontWeight:'700',
+    textDecoration:'underline'
+}
