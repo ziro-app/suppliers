f0dc689a6c9a713243f5a5ef8830658770b68c0c
Antonio Decanini <antonio.decanini@hotmail.com>
2020-12-22 11:40:59 -0300

    fix: fix payment link

--- src/App/Transactions/TransactionDetails/index.js
+++ src/App/Transactions/TransactionDetails/index.js
@@ -23,6 +23,7 @@ import fetch from './fetch';
 import { dateFormat, parcelFormat, round, stringToFloat } from '../utils';
 import { btn, btnRed, buttonContainer, custom, illustrationContainer, modalContainer, modalLabel, spinner } from './styles';
 import { userContext } from '../../appContext';
+import linkMessage from '../../CreatePayment/utils/linkMessage';
 
 const TransactionDetails = ({ transactions, transactionId, transaction, setTransaction, setPayments, transactionsMemo }) => {
     const [receipt_id, setReceipt_id] = useState('');
@@ -44,13 +45,12 @@ const TransactionDetails = ({ transactions, transactionId, transaction, setTrans
     let markupTransaction = {};
     let antiFraudTransaction = {};
 
-    const paymentLink = process.env.HOMOLOG
-    ? transaction.checkoutWithoutRegister
-      ? `http://localhost:8080/pagamento/${transactionId}/finalizar-sem-cadastro`
-      : `http://localhost:8080/pagamento/${transactionId}/escolher-cartao`
-    : transaction.checkoutWithoutRegister
-    ? `https://ziro.app/pagamento/${transactionId}/finalizar-sem-cadastro`
-    : `https://ziro.app/pagamento/${transactionId}/escolher-cartao`
+    const baseUrl = process.env.HOMOLOG ? 'http://localhost:8080/pagamento/' : 'https://ziro.app/pagamento/';
+    const seller = (transaction && transaction.seller) || '';
+    const charge = (transaction && transaction.charge) || '';
+    const installmentsMax = (transaction && transaction.installmentsMax) || '';
+    const checkoutWithoutRegister = (transaction && transaction.checkoutWithoutRegister) || false;
+    const messageLink = linkMessage(baseUrl, transactionId, seller, charge, installmentsMax, checkoutWithoutRegister);
 
     let insuranceValueFormatted = '-';
     let markupValueFormatted = '-';
@@ -363,7 +363,7 @@ const TransactionDetails = ({ transactions, transactionId, transaction, setTrans
     const isWaiting = transaction.status === 'Aguardando Pagamento' || transaction.status === 'Aprovação Pendente';
     return (
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} style={containerWithPadding}>
-            <textarea type="text" style={{ position: 'absolute', left: '-9999px' }} value={paymentLink} ref={textAreaRef} readOnly />
+            <textarea type="text" style={{ position: 'absolute', left: '-9999px' }} value={messageLink} ref={textAreaRef} readOnly />
             <Header type="icon" title="Detalhes da venda" setIsOpen={backRoute ? () => history.push(backRoute, { snapshot: snapshotMemo }) : () => setLocation('/transacoes')} icon="back" />
 
             <div style={{ display: 'grid', gridRowGap: '20px' }}>
