25ce8d6a64a0c8c8531158a0dbe6b5c56167f3c9
Ahmad Forhat <ahmad.forhat@gmail.com>
2021-01-11 17:48:18 -0300

    fix: add conditional to collaborator accont

--- src/App/Transactions/fetchList.js
+++ src/App/Transactions/fetchList.js
@@ -2,26 +2,44 @@ import {db} from '../../Firebase'
 import {removeDuplicates} from './utils'
 
 const fetchList = (state) => {
-    const {setListStatus, setClientList, setFirstDate, setErrorLoading,zoopId} = state
+    const {
+        setListStatus,
+        setClientList,
+        setFirstDate,
+        setErrorLoading,
+        zoopId,
+        isCollaborator,
+        docId
+    } = state
     const run = async () => {
         try {
-            const query = db
-        .collection('credit-card-payments')
-        .orderBy('dateLastUpdate', 'desc')
-        .where('sellerZoopId', '==', zoopId)
-        const listClients = []
-        const listDates = []
-        const listStatus = []
-        const collectionData = await query.get()
-        collectionData.forEach(doc => {
-            listClients.push(doc.data().buyerRazao)
-            listDates.push(doc.data().dateLastUpdate.toDate())
-            listStatus.push(doc.data().status)
-        })
-        setListStatus(removeDuplicates(listStatus.filter(Boolean)))
-        setClientList(removeDuplicates(listClients.filter(Boolean)))
-        const minDate = new Date(Math.min.apply(null,listDates));
-        setFirstDate(minDate)
+            const gerateQuery = () => {
+                if(isCollaborator){
+                    return db
+                    .collection('credit-card-payments')
+                    .orderBy('dateLastUpdate', 'desc')
+                    .where('sellerZoopId', '==', zoopId)
+                    .where('collaboratorId', '==', docId)
+                }
+                return db
+                    .collection('credit-card-payments')
+                    .orderBy('dateLastUpdate', 'desc')
+                    .where('sellerZoopId', '==', zoopId)
+            }
+            const query = gerateQuery()
+            const listClients = []
+            const listDates = []
+            const listStatus = []
+            const collectionData = await query.get()
+            collectionData.forEach(doc => {
+                listClients.push(doc.data().buyerRazao)
+                listDates.push(doc.data().dateLastUpdate.toDate())
+                listStatus.push(doc.data().status)
+            })
+            setListStatus(removeDuplicates(listStatus.filter(Boolean)))
+            setClientList(removeDuplicates(listClients.filter(Boolean)))
+            const minDate = new Date(Math.min.apply(null,listDates));
+            setFirstDate(minDate)
         } catch (error) {
             setErrorLoading(true)
         }
