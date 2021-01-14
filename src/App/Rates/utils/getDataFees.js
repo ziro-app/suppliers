e5158a910d60acb7c0723c18ca8e2d91d0394d48
Antonio Decanini <antonio.decanini@hotmail.com>
2021-01-14 01:18:01 -0300

    fix: fix installments info

--- src/App/Rates/utils/getDataFees.js
+++ src/App/Rates/utils/getDataFees.js
@@ -10,13 +10,13 @@ const arrayDatas = (dataPlan, brands, activePlan) => {
         for(let i = 0; i <= 12; i++){
             const fullNameFluxo = {
                 0: 'débito',
-                1: 'a vista',
+                1: 'à vista',
                 2: '2x a 6x',
                 7: '7x a 12x'
             }
             const fullNameNormal = {
                 0: 'débito',
-                1: 'a vista'
+                1: 'à vista'
             }
             const conditionalFluxo = i === 0 || i === 1 ||  i === 2 || i === 7
             const conditionalAntecipado = true
