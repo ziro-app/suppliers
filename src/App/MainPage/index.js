8b4b494cdaaee32b03894fdf70eab4bbe09e53b2
Bruno Bulgaron <brunohbulgaron@gmail.com>
2021-01-21 16:51:55 -0300

    style: added card styles at MainPage

--- src/App/MainPage/index.js
+++ src/App/MainPage/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { useLocation } from 'wouter'
+import { useLocation } from 'wouter';
 import Icon from '@bit/vitorbarbosa19.ziro.icon';
 import Spinner from '@bit/vitorbarbosa19.ziro.spinner';
 import { container } from '@ziro/theme';
@@ -13,141 +13,154 @@ import getActivePlan from './utils/getActivePlan';
 import Skeleton from 'react-loading-skeleton';
 
 function MainPage() {
-    const { role, zoopId, payoutAutomatic, fantasy, uid } = useContext(userContext)
-    const [, setLocation] = useLocation();
+  const { role, zoopId, payoutAutomatic, fantasy, uid } = useContext(userContext);
+  const [, setLocation] = useLocation();
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [errorLoading, setErrorLoading] = useState(false);
-    const [customError, setCustomError] = useState(false);
-    const [receivables, setReceivables] = useState([]);
-    const [data, setData] = useState([]);
-    const [hasMore, setHasMore] = useState(true);
-    const [loadingMore, setLoadingMore] = useState(false);
-    const [initDate, setInitDate] = useState(new Date());
-    const [finalDate, setFinalDate] = useState();
-    const [totalAmount, setTotalAmount] = useState(0);
-    const [totalTransactions, setTotalTransactions] = useState(0);
-    const [days, setDays] = useState(0);
-    const [balance, setBalance] = useState(-1);
-    const [paidBalance, setPaidBalance] = useState(-1);
-    const [activePlan, setActivePlan] = useState('');
-    const setState = {
-        setIsLoading, setErrorLoading, setReceivables, setData, setLocation,
-        setHasMore, setLoadingMore, setInitDate, setFinalDate, setTotalAmount,
-        setDays, setCustomError, setTotalTransactions, setBalance, setPaidBalance
-    };
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorLoading, setErrorLoading] = useState(false);
+  const [customError, setCustomError] = useState(false);
+  const [receivables, setReceivables] = useState([]);
+  const [data, setData] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [initDate, setInitDate] = useState(new Date());
+  const [finalDate, setFinalDate] = useState();
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [totalTransactions, setTotalTransactions] = useState(0);
+  const [days, setDays] = useState(0);
+  const [balance, setBalance] = useState(-1);
+  const [paidBalance, setPaidBalance] = useState(-1);
+  const [activePlan, setActivePlan] = useState('');
+  const setState = {
+    setIsLoading,
+    setErrorLoading,
+    setReceivables,
+    setData,
+    setLocation,
+    setHasMore,
+    setLoadingMore,
+    setInitDate,
+    setFinalDate,
+    setTotalAmount,
+    setDays,
+    setCustomError,
+    setTotalTransactions,
+    setBalance,
+    setPaidBalance,
+  };
 
-    const getPlan = async () => {
-        let result = await getActivePlan(uid);
-        return setActivePlan(result === 'standard' ? 'Fluxo' : result === 'financed30' ? 'Antecipado D+30' : result === 'financed14' ? 'Antecipado D+14' : 'Não encontrado.');
-    };
+  const getPlan = async () => {
+    let result = await getActivePlan(uid);
+    return setActivePlan(result === 'standard' ? 'Fluxo' : result === 'financed30' ? 'Antecipado D+30' : result === 'financed14' ? 'Antecipado D+14' : 'Não encontrado.');
+  };
 
-    useEffect(() => {
-        fetchBalance(zoopId, setState);
-        getPlan();
-        // fetch(zoopId, initDate, totalAmount, totalTransactions, data, days, receivables, fantasy, setState);
-    }, [])
+  useEffect(() => {
+    fetchBalance(zoopId, setState);
+    getPlan();
+    // fetch(zoopId, initDate, totalAmount, totalTransactions, data, days, receivables, fantasy, setState);
+  }, []);
 
-    return (
-        <div style={container}>
-
-            <div style={saldosContainer}>
-                <div style={card}>
-                    <label style={saldosLabel}>Plano ativo</label>
-                    <h1 style={valorH1}>
-                        {activePlan === '' ?
-                            <Skeleton width={150} height={30} />
-                            :
-                            activePlan
-                        }
-                    </h1>
-                </div>
-
-                {role === '' &&
-                    <>
-                        <div style={card}>
-                            <label style={saldosLabel}>Saldo à receber hoje</label>
-                            <h1 style={valorH1}>
-                                {balance < 0 ?
-                                    <Skeleton width={150} height={30} />
-                                    :
-                                    balance ?
-                                        currencyFormat(round(balance, 2).toFixed(2).replace('.', '')) : 'R$ 0,00'}
-                            </h1>
-                        </div>
-
-                        <div style={card}>
-                            <label style={saldosLabel}>Saldo pago hoje</label>
-                            <h1 style={valorH1}>
-                                {paidBalance < 0 ?
-                                    <Skeleton width={150} height={30} />
-                                    :
-                                    paidBalance ?
-                                        currencyFormat(round(paidBalance, 2).toFixed(2).replace('.', '')) : 'R$ 0,00'}
-                            </h1>
-                        </div>
-                    </>
-                }
-            </div>
-
-            <div style={iconsContainer}>
-                <div>
-                    <div style={iconDiv}>
-                        <Icon type='money' size={20} strokeWidth={2} style={iconStyle} onClick={() => setLocation('/criar-cobranca')} />
-                    </div>
-
-                    <div style={iconDescription}>
-                        <label style={{ fontSize: '1.3rem' }}>Cobrar</label>
-                    </div>
-                </div>
-                <div>
-                    <div style={iconDiv}>
-                        <Icon type='trending' size={20} strokeWidth={2} style={iconStyle} onClick={() => setLocation('/transacoes')} />
-                    </div>
-
-                    <div style={iconDescription}>
-                        <label style={{ fontSize: '1.3rem' }}>Vendas</label>
-                    </div>
-                </div>
-                <div>
-                    <div style={iconDiv}>
-                        <Icon type='id' size={20} strokeWidth={2} style={iconStyle} onClick={() => setLocation('/recebiveis')} />
-                    </div>
-
-                    <div style={iconDescription}>
-                        <label style={{ fontSize: '1.3rem' }}>Recebíveis</label>
-                    </div>
-                </div>
-                <div>
-                    <div style={iconDiv}>
-                        <Icon type='library' size={20} strokeWidth={2} style={iconStyle} onClick={() => setLocation('/recebiveis/dados-bancarios')} />
-                    </div>
-
-                    <div style={iconDescription}>
-                        <label style={{ fontSize: '1.3rem' }}>Dados<br />Bancários</label>
-                    </div>
-                </div>
-                <div>
-                    <div style={iconDiv}>
-                        <Icon type='user' size={20} strokeWidth={2} style={iconStyle} onClick={() => setLocation('/colaboradores')} />
-                    </div>
-
-                    <div style={iconDescription}>
-                        <label style={{ fontSize: '1.3rem' }}>Vendedores</label>
-                    </div>
-                </div>
-                <div>
-                    <div style={iconDiv}>
-                        <Icon type='gear' size={20} strokeWidth={2} style={iconStyle} onClick={() => setLocation('/minha-conta')} />
-                    </div>
-
-                    <div style={iconDescription}>
-                        <label style={{ fontSize: '1.3rem' }}>Dados<br />Gerais</label>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div style={container}>
+      <div style={saldosContainer}>
+        <div style={card}>
+          <label style={saldosLabel}>Plano ativo</label>
+          <h1 style={valorH1}>{activePlan === '' ? <Skeleton width={150} height={30} /> : activePlan}</h1>
         </div>
-    )
+
+        {role === '' && (
+          <>
+            <div style={card}>
+              <label style={saldosLabel}>Saldo à receber hoje</label>
+              <h1 style={valorH1}>
+                {balance < 0 ? 
+                  <Skeleton width={150} height={30} /> 
+                  : balance ? currencyFormat(round(balance, 2).toFixed(2).replace('.', '')) 
+                  : 'R$ 0,00'
+                }
+              </h1>
+            </div>
+
+            <div style={card}>
+              <label style={saldosLabel}>Saldo pago hoje</label>
+              <h1 style={valorH1}>
+                {paidBalance < 0 ? 
+                  <Skeleton width={150} height={30} />
+                  : paidBalance ? currencyFormat(round(paidBalance, 2).toFixed(2).replace('.', '')) 
+                  : 'R$ 0,00'
+                }
+              </h1>
+            </div>
+          </>
+        )}
+      </div>
+
+      <div style={iconsContainer}>
+        <div>
+          <div style={iconDiv}>
+            <Icon type="money" size={20} strokeWidth={2} style={iconStyle} onClick={() => setLocation('/criar-cobranca')} />
+          </div>
+
+          <div style={iconDescription}>
+            <label style={{ fontSize: '1.3rem' }}>Cobrar</label>
+          </div>
+        </div>
+        <div>
+          <div style={iconDiv}>
+            <Icon type="trending" size={20} strokeWidth={2} style={iconStyle} onClick={() => setLocation('/transacoes')} />
+          </div>
+
+          <div style={iconDescription}>
+            <label style={{ fontSize: '1.3rem' }}>Vendas</label>
+          </div>
+        </div>
+        <div>
+          <div style={iconDiv}>
+            <Icon type="id" size={20} strokeWidth={2} style={iconStyle} onClick={() => setLocation('/recebiveis')} />
+          </div>
+
+          <div style={iconDescription}>
+            <label style={{ fontSize: '1.3rem' }}>Recebíveis</label>
+          </div>
+        </div>
+        <div>
+          <div style={iconDiv}>
+            <Icon type="library" size={20} strokeWidth={2} style={iconStyle} onClick={() => setLocation('/recebiveis/dados-bancarios')} />
+          </div>
+
+          <div style={iconDescription}>
+            <label style={{ fontSize: '1.3rem' }}>
+              Dados
+              <br />
+              Bancários
+            </label>
+          </div>
+        </div>
+        <div>
+          <div style={iconDiv}>
+            <Icon type="user" size={20} strokeWidth={2} style={iconStyle} onClick={() => setLocation('/colaboradores')} />
+          </div>
+
+          <div style={iconDescription}>
+            <label style={{ fontSize: '1.3rem' }}>Vendedores</label>
+          </div>
+        </div>
+        <div>
+          <div style={iconDiv}>
+            <Icon type="gear" size={20} strokeWidth={2} style={iconStyle} onClick={() => setLocation('/minha-conta')} />
+          </div>
+
+          <div style={iconDescription}>
+            <label style={{ fontSize: '1.3rem' }}>
+              Dados
+              <br />
+              Gerais
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MainPage
+export default MainPage;
--- src/App/MainPage/styles.js
+++ src/App/MainPage/styles.js
@@ -1,53 +1,62 @@
 import { fontTitle, fontSizeSmall } from '@ziro/theme'
 
 export const saldosContainer = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: '-10px'
+  // display: 'flex',
+  // flexDirection: 'column',
+  // justifyContent: 'center',
+  // alignItems: 'center',
+  display: 'grid',
+  placeItems: 'center',
+  rowGap: '50px',
+  marginTop: '-10px'
 },
-    card = {
-        textAlign: 'center',
-        width: '70%',
-        padding: '20px',
-        borderRadius: '30px',
-        marginBottom: '-22px'
-    },
-    saldosLabel = {
-        opacity: '50%',
-        fontSize: '1.3rem'
-    },
-    activePlan = {
-        opacity: '60%',
-    },
-    iconsContainer = {
-        display: 'grid',
-        gridTemplateColumns: '1fr 1fr 1fr',
-        marginTop: '50px'
-    },
-    valorH1 = {
-        textAlign: 'center',
-        fontSize: '3rem',
-        margin: '0',
-    },
-    iconDiv = {
-        display: 'grid',
-        placeItems: 'center',
-        cursor: 'pointer'
-    },
-    iconStyle = {
-        borderRadius: '40%',
-        padding: '20px',
-        boxShadow: 'rgba(34, 34, 34, 0.7) 0px 3px 11px -4px',
-    },
-    iconDescription = {
-        marginTop: '10px',
-        marginBottom: '30px',
-        textAlign: 'center',
-        opacity: '80%'
-    },
-    spinnerStyle = {
-        display: 'grid',
-        justifyItems: 'center'
-    }
+card = {
+  textAlign: 'center',
+  width: '100%',
+  padding: '20px 0px',
+  borderRadius: '30px',
+  marginBottom: '-22px',
+  boxShadow: 'rgba(34, 34, 34, 0.4) 0px 3px 11px -4px',
+},
+saldosLabel = {
+  opacity: '50%',
+  fontSize: '1.3rem'
+},
+activePlan = {
+  opacity: '60%',
+},
+iconsContainer = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(auto-fill, minmax(100px, 137px))',
+  // gridAutoColumns: 'minmax(auto, auto)',
+  gridColumnGap: '6px',
+  justifyContent: 'space-between',
+  marginTop: '50px'
+},
+iconDivContainer = {
+  // width: '100%'
+},
+valorH1 = {
+  textAlign: 'center',
+  fontSize: '3rem',
+  margin: '0',
+},
+iconDiv = {
+  // display: 'grid',
+  // gridTemplateColumns: 'auto auto',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  cursor: 'pointer'
+},
+iconStyle = {
+  borderRadius: '40%',
+  padding: '20px',
+  boxShadow: 'rgba(34, 34, 34, 0.7) 0px 3px 11px -4px',
+},
+iconDescription = {
+  marginTop: '10px',
+  marginBottom: '30px',
+  textAlign: 'center',
+  opacity: '80%'
+}
