c2ef9d9ec5cf27f2c1066df4e82667354f680d7c
Wermeson Rocha <wermesonrocha2@gmail.com>
2021-02-05 18:30:38 -0300

    refactor: Using transaction endpoint to fetch the values received and receivable for the day

--- src/App/MainPage/fetchBalance.js
+++ src/App/MainPage/fetchBalance.js
@@ -8,29 +8,39 @@ const config = {
 };
 
 
-const formatDate = date => `${date.getFullYear()}-${date.getMonth() + 1 <= 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1}-${date.getDate() <= 9 ? `0${date.getDate()}` : date.getDate()}`;
+const formatDate = strDate => {
+    const date = new Date(strDate);
+    date.setHours(date.getHours() + 3);
+    return date;
+};
 
 const fetchBalance = (zoopId, { setBalance, setPaidBalance, setIsErrorBalance }) => {
     const source = axios.CancelToken.source();
     const run = async () => {
         try {
-            const balanceUrl = `${process.env.PAY_URL}account-balance-by-seller?seller_id=${zoopId}`;
-            // const balanceUrl = `http://localhost:9003/.netlify/functions/account-balance-by-seller?seller_id=${zoopId}`;
-            const { data: { items: { current_balance } } } = await post(balanceUrl, {}, config);
-            const parts = current_balance.split('.');
-            const parsed = parseFloat(parts[0]) / 100;
-            const rounded = (parseFloat(round(parsed, 2).toFixed(2)));
-            setBalance(rounded);
-
-            const formatted = formatDate(new Date());
-            const historyUrl = `${process.env.PAY_URL}account-history-all?seller_id=${zoopId}&created_date_range[gte]=${formatted}&created_date_range[lte]=${formatted}`;
-            // const historyUrl = `http://localhost:9003/.netlify/functions/account-history-all?seller_id=${zoopId}&created_date_range[gte]=${formatted}&created_date_range[lte]=${formatted}`;
-            const { data: { items } } = await post(historyUrl, {}, config);
-            if (Object.prototype.hasOwnProperty.call(items, formatted)) {
-                const floatValue = items[formatted]['items'].map(it => parseFloat(it.amount) < 0 ? -parseFloat(it.amount) : parseFloat(it.amount)).reduce((a, b) => a + b);
-                const paidRounded = parseFloat(round(floatValue, 2).toFixed(2));
-                setPaidBalance(paidRounded);
-            } else setPaidBalance(0);
+            let hasMore = true;
+            let offset = 0;
+            let arrayItems = [];
+            const now = new Date();
+            now.setHours(0, 0, 0, 0);
+            while (hasMore) {
+                let url = `${process.env.PAY_URL}sellers-transfers?seller_id=${zoopId}&offset=${offset}&limit=40`;
+                const { data } = await post(url, {}, config);
+                const { items, has_more } = data;
+                arrayItems = [...arrayItems, ...items];
+                const finalItem = items[items.length - 1];
+                const finalDate = formatDate(finalItem['transfer_date']);
+                if (has_more && now.getTime() === finalDate.getTime()) {
+                    offset += 40;
+                } else hasMore = false;
+            }
+            const interest = arrayItems.filter(({ transfer_date }) => now.getTime() === formatDate(transfer_date).getTime());
+            const unpaidAmount = interest.filter(({ confirmed }) => confirmed == 0).map(({ amount }) => parseFloat(amount)).reduce((a, b) => a + b, 0);
+            const paidAmount = interest.filter(({ confirmed }) => confirmed == 1).map(({ amount }) => parseFloat(amount)).reduce((a, b) => a + b, 0);
+            console.log('unpaidAmount: ', parseFloat(round(unpaidAmount, 2).toFixed(2)));
+            console.log('paidAmount: ', parseFloat(round(paidAmount, 2).toFixed(2)));
+            setBalance(unpaidAmount);
+            setPaidBalance(paidAmount);
         } catch (error) {
             setIsErrorBalance(true)
             if (error.response) console.log(error.response);
