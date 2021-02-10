3215530f78a81c1dcee6687f3f40aebe3530de30
Wermeson Rocha <wermesonrocha2@gmail.com>
2021-02-10 09:56:52 -0300

    chore: Creating a utility for querying the day's transactions and removing unused files

--- src/App/MainPage/fetchBalance.js
+++ src/App/MainPage/fetchBalance.js
@@ -1,42 +1,14 @@
-import axios, { post } from 'axios';
+import axios from 'axios';
 import { round } from '../Transactions/utils';
-
-const config = {
-    headers: {
-        Authorization: `${process.env.PAY_TOKEN}`,
-    }
-};
-
-
-const formatDate = strDate => {
-    const date = new Date(strDate);
-    date.setHours(date.getHours() + 3);
-    return date;
-};
+import fetchTransactionsOfDay from '../utils/fetchTransactionsOfDay';
 
 const fetchBalance = (zoopId, { setBalance, setPaidBalance, setIsErrorBalance }) => {
     const source = axios.CancelToken.source();
     const run = async () => {
         try {
-            let hasMore = true;
-            let offset = 0;
-            let arrayItems = [];
-            const now = new Date();
-            now.setHours(0, 0, 0, 0);
-            while (hasMore) {
-                let url = `${process.env.PAY_URL}sellers-transfers?seller_id=${zoopId}&offset=${offset}&limit=40`;
-                const { data } = await post(url, {}, config);
-                const { items, has_more } = data;
-                arrayItems = [...arrayItems, ...items];
-                const finalItem = items[items.length - 1];
-                const finalDate = formatDate(finalItem['transfer_date']);
-                if (has_more && now.getTime() === finalDate.getTime()) {
-                    offset += 40;
-                } else hasMore = false;
-            }
-            const interest = arrayItems.filter(({ transfer_date }) => now.getTime() === formatDate(transfer_date).getTime());
-            const unpaidAmount = interest.filter(({ confirmed }) => confirmed == 0).map(({ amount }) => parseFloat(amount)).reduce((a, b) => a + b, 0);
-            const paidAmount = interest.filter(({ confirmed }) => confirmed == 1).map(({ amount }) => parseFloat(amount)).reduce((a, b) => a + b, 0);
+            const transactions = await fetchTransactionsOfDay(zoopId);
+            const unpaidAmount = transactions.filter(({ confirmed }) => confirmed == 0).map(({ amount }) => parseFloat(amount)).reduce((a, b) => a + b, 0);
+            const paidAmount = transactions.filter(({ confirmed }) => confirmed == 1).map(({ amount }) => parseFloat(amount)).reduce((a, b) => a + b, 0);
             console.log('unpaidAmount: ', parseFloat(round(unpaidAmount, 2).toFixed(2)));
             console.log('paidAmount: ', parseFloat(round(paidAmount, 2).toFixed(2)));
             setBalance(unpaidAmount);
--- src/App/Receivables/fetchBalance.js
+++ src/App/Receivables/fetchBalance.js
@@ -1,36 +1,13 @@
-import axios, { post } from 'axios';
-import { round } from '../Transactions/utils';
-
-const config = {
-    headers: {
-        Authorization: `${process.env.PAY_TOKEN}`,
-    }
-};
-
-
-const formatDate = date => `${date.getFullYear()}-${date.getMonth() + 1 <= 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1}-${date.getDate() <= 9 ? `0${date.getDate()}` : date.getDate()}`;
+import axios from 'axios';
+import fetchTransactionsOfDay from '../utils/fetchTransactionsOfDay';
 
 const fetchBalance = (zoopId, { setBalance }) => {
     const source = axios.CancelToken.source();
     const run = async () => {
         try {
-            const balanceUrl = `${process.env.PAY_URL}account-balance-by-seller?seller_id=${zoopId}`;
-            const { data: { items: { current_balance } } } = await post(balanceUrl, {}, config);
-            const parts = current_balance.split('.');
-            const parsed = parseFloat(parts[0]) / 100;
-            if (parsed !== 0) {
-                const rounded = (parseFloat(round(parsed, 2).toFixed(2)));
-                setBalance(rounded);
-            } else {
-                const formatted = formatDate(new Date());
-                const historyUrl = `${process.env.PAY_URL}account-history-all?seller_id=${zoopId}&created_date_range[gte]=${formatted}&created_date_range[lte]=${formatted}`;
-                const { data: { items } } = await post(historyUrl, {}, config);
-                if (Object.prototype.hasOwnProperty.call(items, formatted)) {
-                    const floatValue = items[formatted]['items'].map(it => parseFloat(it.amount) < 0 ? -parseFloat(it.amount) : parseFloat(it.amount)).reduce((a, b) => a + b);
-                    const rounded = parseFloat(round(floatValue, 2).toFixed(2));
-                    setBalance(rounded);
-                }
-            }
+            const transactions = await fetchTransactionsOfDay(zoopId);
+            const unpaidAmount = transactions.filter(({ confirmed }) => confirmed == 0).map(({ amount }) => parseFloat(amount)).reduce((a, b) => a + b, 0);
+            setBalance(unpaidAmount);
         } catch (error) {
             if (error.response) console.log(error.response);
             else console.log(error);
