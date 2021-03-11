cc04a125c4a17888385c2e2461ca551caf5fd653
Wermeson Rocha <wermesonrocha2@gmail.com>
2021-03-11 08:54:18 -0300

    fix: Added object?.property notation to prevent errors and minor fixes

--- src/App/BackgroundCheck/utils/mountBlocks.js
+++ src/App/BackgroundCheck/utils/mountBlocks.js
@@ -17,8 +17,6 @@ const mountBlockPF = (data, setPages) => {
     const { nome, dataNascimento, socios,
         falecido, idade, situacaoCpf, dataSituacaoCpf, nacionalidade, numDependentes,
         genero, pessoaPolitica, rendaPresumida, telefones, emails, enderecos, parentescos } = data;
-    const { classeFamilia, classeIndividual, descricaoFamilia, descricaoIndividual,
-        salarioFamiliar, salary } = rendaPresumida;
     const { cheque, dividas_vencidas, protestos, pefin, refin, ccf } = data;
     const { setPendency, setDefaultData } = setPages;
     return [
@@ -47,7 +45,7 @@ const mountBlockPF = (data, setPages) => {
                 },
                 {
                     title: 'Nº dependentes',
-                    content: numDependentes
+                    content: numDependentes || 'Não informado'
                 },
                 {
                     title: 'Pessoa política',
@@ -59,15 +57,15 @@ const mountBlockPF = (data, setPages) => {
                 },
                 {
                     title: 'Parentescos',
-                    content: (parentescos && parentescos.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...parentescos, nome: 'Parentescos', sortedFields: ["nome", "cpf", "parentesco", "genero"] })}>Visualizar</div> : 'Nada consta'
+                    content: (parentescos && parentescos.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...parentescos, nome: 'Parentescos', sortedFields: ["nome", "cpf", "parentesco", "genero"] })}>Visualizar</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>Endereços&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Endereços" body={addressBody} /></div></div>,
-                    content: (enderecos && enderecos.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...enderecos, nome: 'Endereços', sortedFields: ["cep", "rua", "numero", "complemento", "bairro", "cidade", "estado"] })}>Visualizar</div> : 'Nada consta'
+                    content: (enderecos && enderecos.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...enderecos, nome: 'Endereços', sortedFields: ["cep", "rua", "numero", "complemento", "bairro", "cidade", "estado"] })}>Visualizar</div> : 'Não informado'
                 },
                 {
                     title: 'Part. societárias',
-                    content: (socios && socios.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...socios, nome: 'Participações societárias', sortedFields: ["capital"] })}>Visualizar</div> : 'Nada consta'
+                    content: (socios && socios.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...socios, nome: 'Participações societárias', sortedFields: ["capital"] })}>Visualizar</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>Situação Cadastral&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Situação Cadastral" body={cadastralSituationBody} /></div></div>,
@@ -85,11 +83,11 @@ const mountBlockPF = (data, setPages) => {
             body: [
                 {
                     title: <div style={tooltipContainer}>Telefones&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Telefones" body={phonesBody} /></div></div>,
-                    content: (telefones && telefones.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...telefones, nome: 'Telefones', sortedFields: ["ddd", "numero"], blockTitle: 'Telefone' })}>Visualizar</div> : 'Nada consta'
+                    content: (telefones && telefones.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...telefones, nome: 'Telefones', sortedFields: ["ddd", "numero"], blockTitle: 'Telefone' })}>Visualizar</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>Emails&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Emails" body={emailsBody} /></div></div>,
-                    content: (emails && emails.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...emails, nome: 'Emails', sortedFields: ["email"], blockTitle: 'Email' })}>Visualizar</div> : 'Nada consta'
+                    content: (emails && emails.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...emails, nome: 'Emails', sortedFields: ["email"], blockTitle: 'Email' })}>Visualizar</div> : 'Não informado'
                 }
             ]
         },
@@ -97,20 +95,20 @@ const mountBlockPF = (data, setPages) => {
             header: 'Renda Presumida',
             body: [
                 {
-                    title: <div style={tooltipContainer}>Classe I&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Classe individual" body={economicClassBody(descricaoIndividual)} /></div></div>,
-                    content: classeIndividual || 'Não encontrado'
+                    title: <div style={tooltipContainer}>Classe I&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Classe individual" body={economicClassBody(rendaPresumida?.descricaoIndividual || 'Descrição não informada')} /></div></div>,
+                    content: rendaPresumida?.classeIndividual || 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>Salário I&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Salário individual" body={individualSalaryBody} /></div></div>,
-                    content: formatCurrency(salary)
+                    content: formatCurrency(rendaPresumida?.salary || '')
                 },
                 {
-                    title: <div style={tooltipContainer}>Classe F&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Classe familiar" body={economicClassBody(descricaoFamilia)} /></div></div>,
-                    content: classeFamilia || 'Não encontrado'
+                    title: <div style={tooltipContainer}>Classe F&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Classe familiar" body={economicClassBody(rendaPresumida?.descricaoFamilia || 'Descrição não informada')} /></div></div>,
+                    content: rendaPresumida?.classeFamilia || 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>Salário F&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Salário familiar" body={familySalaryBody} /></div></div>,
-                    content: formatCurrency(salarioFamiliar)
+                    content: formatCurrency(rendaPresumida?.salarioFamiliar || '')
                 }
             ]
         },
@@ -119,27 +117,27 @@ const mountBlockPF = (data, setPages) => {
             body: [
                 {
                     title: 'Dívidas vencidas',
-                    content: (dividas_vencidas && dividas_vencidas.count > 0) ? <div style={details} onClick={() => setPendency({ ...dividas_vencidas, nome: 'Dividas Vencidas' })}>Pendências</div> : 'Nada consta'
+                    content: (dividas_vencidas && dividas_vencidas.count > 0) ? <div style={details} onClick={() => setPendency({ ...dividas_vencidas, nome: 'Dividas Vencidas' })}>Pendências</div> : 'Não informado'
                 },
                 {
                     title: 'Cheque',
-                    content: (cheque && cheque.count > 0) ? <div style={details} onClick={() => setPendency({ ...dividas_vencidas, nome: 'Cheque' })}>Pendências</div> : 'Nada consta'
+                    content: (cheque && cheque.count > 0) ? <div style={details} onClick={() => setPendency({ ...dividas_vencidas, nome: 'Cheque' })}>Pendências</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>Protestos&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Protestos" body={protestsBody} /></div></div>,
-                    content: (protestos && protestos.count > 0) ? <div style={details} onClick={() => setPendency({ ...protestos, nome: 'Protestos' })}>Pendências</div> : 'Nada consta'
+                    content: (protestos && protestos.count > 0) ? <div style={details} onClick={() => setPendency({ ...protestos, nome: 'Protestos' })}>Pendências</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>PEFIN&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="PEFIN" body={pefinBody} /></div></div>,
-                    content: (pefin && pefin.count > 0) ? <div style={details} onClick={() => setPendency({ ...pefin, nome: 'PEFIN' })}>Pendências</div> : 'Nada consta'
+                    content: (pefin && pefin.count > 0) ? <div style={details} onClick={() => setPendency({ ...pefin, nome: 'PEFIN' })}>Pendências</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>REFIN&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="REFIN" body={refinBody} /></div></div>,
-                    content: (refin && refin.count > 0) ? <div style={details} onClick={() => setPendency({ ...refin, nome: 'REFIN' })}>Pendências</div> : 'Nada consta'
+                    content: (refin && refin.count > 0) ? <div style={details} onClick={() => setPendency({ ...refin, nome: 'REFIN' })}>Pendências</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>CCF&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="CCF" body={ccfBody} /></div></div>,
-                    content: (ccf && ccf.count > 0) ? <div style={details} onClick={() => setPendency({ ...ccf, nome: 'CCF' })}>Pendências</div> : 'Nada consta'
+                    content: (ccf && ccf.count > 0) ? <div style={details} onClick={() => setPendency({ ...ccf, nome: 'CCF' })}>Pendências</div> : 'Não informado'
                 }
             ]
         }
@@ -149,11 +147,9 @@ const mountBlockPF = (data, setPages) => {
 const mountBlockPJ = (data, setPages) => {
     const { razao, dataFundacao, situacaoCadastral, dataSituacaoCnpj, telefones,
         ramoAtv, naturezaJuridica, tipo, dividas_vencidas, rendimentos, emails, enderecos,
-        protestos, pefin, refin, ccf, socios, simplesNacional, porte, administradores } = data;
+        protestos, pefin, refin, ccf, simplesNacional, porte, administradores } = data;
     const { setPendency, setDefaultData } = setPages;
-    const { capitalSocial, balancoMinimo, balancoMaximo, faturamentoMinimo, faturamentoMaximo } = rendimentos;
-    const normalized = porte && capitalize(porte.split('_').join(' ').trim()) || 'Não informado'
-    console.log(data);
+    const normalized = porte && capitalize(porte.split('_').join(' ').trim()) || 'Não informado';
     return [
         {
             header: 'Informações do Documento',
@@ -180,11 +176,11 @@ const mountBlockPJ = (data, setPages) => {
                 },
                 {
                     title: <div style={tooltipContainer}>Simples N.&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Simples Nacional" body={simpleNationalBody} /></div></div>,
-                    content: simplesNacional === 'N' ? 'Não' : 'Sim'
+                    content: simplesNacional ? simplesNacional === 'N' ? 'Não' : 'Sim' : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>Endereços&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Endereços" body={addressBody} /></div></div>,
-                    content: (enderecos && enderecos.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...enderecos, nome: 'Endereços', sortedFields: ["cep", "rua", "numero", "complemento", "bairro", "cidade", "estado"], pj: true })}>Visualizar</div> : 'Nada consta'
+                    content: (enderecos && enderecos.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...enderecos, nome: 'Endereços', sortedFields: ["cep", "rua", "numero", "complemento", "bairro", "cidade", "estado"], pj: true })}>Visualizar</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>Ramos de Atvd&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Ramos de Atividade" body={fieldsOfActivityBody} /></div></div>,
@@ -192,7 +188,7 @@ const mountBlockPJ = (data, setPages) => {
                 },
                 {
                     title: 'Sócios',
-                    content: (socios && socios.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...administradores, nome: 'Sócios Administradores', sortedFields: ["cpf"], blockTitle: 'Sócio' })}>Visualizar</div> : 'Não informado'
+                    content: (administradores && administradores.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...administradores, nome: 'Sócios Administradores', sortedFields: ["cpf"], blockTitle: 'Sócio' })}>Visualizar</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>Situação Cadastral&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Situação Cadastral" body={cadastralSituationBody} /></div></div>,
@@ -210,11 +206,11 @@ const mountBlockPJ = (data, setPages) => {
             body: [
                 {
                     title: <div style={tooltipContainer}>Telefones&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Telefones" body={phonesBody} /></div></div>,
-                    content: (telefones && telefones.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...telefones, nome: 'Telefones', sortedFields: ["ddd", "numero"], blockTitle: 'Telefone' })}>Visualizar</div> : 'Nada consta'
+                    content: (telefones && telefones.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...telefones, nome: 'Telefones', sortedFields: ["ddd", "numero"], blockTitle: 'Telefone' })}>Visualizar</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>Emails&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Emails" body={emailsBody} /></div></div>,
-                    content: (emails && emails.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...emails, nome: 'Emails', sortedFields: ["email"], blockTitle: 'Email', pj: true })}>Visualizar</div> : 'Nada consta'
+                    content: (emails && emails.count > 0) ? <div style={details} onClick={() => setDefaultData({ ...emails, nome: 'Emails', sortedFields: ["email"], blockTitle: 'Email', pj: true })}>Visualizar</div> : 'Não informado'
                 }
             ]
         },
@@ -223,23 +219,23 @@ const mountBlockPJ = (data, setPages) => {
             body: [
                 {
                     title: <div style={tooltipContainer}>Capital S.&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Capital Social" body={shareCapital} /></div></div>,
-                    content: formatCurrency(capitalSocial)
+                    content: formatCurrency(rendimentos?.capitalSocial || '')
                 },
                 {
                     title: 'Balanço Min.',
-                    content: formatCurrency(balancoMinimo)
+                    content: formatCurrency(rendimentos?.balancoMinimo || '')
                 },
                 {
                     title: 'Faturamento Min.',
-                    content: formatCurrency(faturamentoMinimo)
+                    content: formatCurrency(rendimentos?.faturamentoMinimo || '')
                 },
                 {
                     title: 'Balanço Max.',
-                    content: formatCurrency(balancoMaximo)
+                    content: formatCurrency(rendimentos?.balancoMaximo || '')
                 },
                 {
                     title: 'Faturamento Max.',
-                    content: formatCurrency(faturamentoMaximo)
+                    content: formatCurrency(rendimentos?.faturamentoMaximo || '')
                 },
             ]
         },
@@ -248,23 +244,23 @@ const mountBlockPJ = (data, setPages) => {
             body: [
                 {
                     title: 'Dívidas vencidas',
-                    content: (dividas_vencidas && dividas_vencidas.count > 0) ? <div style={details} onClick={() => setPendency({ ...dividas_vencidas, nome: 'Dividas Vencidas' })}>Pendências</div> : 'Nada consta'
+                    content: (dividas_vencidas && dividas_vencidas.count > 0) ? <div style={details} onClick={() => setPendency({ ...dividas_vencidas, nome: 'Dividas Vencidas' })}>Pendências</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>Protestos&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="Protestos" body={protestsBody} /></div></div>,
-                    content: (protestos && protestos.count > 0) ? <div style={details} onClick={() => setPendency({ ...protestos, nome: 'Protestos' })}>Pendências</div> : 'Nada consta'
+                    content: (protestos && protestos.count > 0) ? <div style={details} onClick={() => setPendency({ ...protestos, nome: 'Protestos' })}>Pendências</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>PEFIN&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="PEFIN" body={pefinBody} /></div></div>,
-                    content: (pefin && pefin.count > 0) ? <div style={details} onClick={() => setPendency({ ...pefin, nome: 'PEFIN' })}>Pendências</div> : 'Nada consta'
+                    content: (pefin && pefin.count > 0) ? <div style={details} onClick={() => setPendency({ ...pefin, nome: 'PEFIN' })}>Pendências</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>REFIN&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="REFIN" body={refinBody} /></div></div>,
-                    content: (refin && refin.count > 0) ? <div style={details} onClick={() => setPendency({ ...refin, nome: 'REFIN' })}>Pendências</div> : 'Nada consta'
+                    content: (refin && refin.count > 0) ? <div style={details} onClick={() => setPendency({ ...refin, nome: 'REFIN' })}>Pendências</div> : 'Não informado'
                 },
                 {
                     title: <div style={tooltipContainer}>CCF&nbsp;<div style={tooltipCursor}><TooltipHelp iconSize={14} illustration="cardAnalysis" title="CCF" body={ccfBody} /></div></div>,
-                    content: (ccf && ccf.count > 0) ? <div style={details} onClick={() => setPendency({ ...ccf, nome: 'CCF' })}>Pendências</div> : 'Nada consta'
+                    content: (ccf && ccf.count > 0) ? <div style={details} onClick={() => setPendency({ ...ccf, nome: 'CCF' })}>Pendências</div> : 'Não informado'
                 }
             ]
         }
