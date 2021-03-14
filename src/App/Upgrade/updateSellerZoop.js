a9c35517ed71bf16f476df7053fdaea89a288963
Wermeson Rocha <wermesonrocha2@gmail.com>
2021-03-14 16:48:34 -0300

    chore: Adding new fields to the zoop

--- src/App/Upgrade/updateSellerZoop.js
+++ src/App/Upgrade/updateSellerZoop.js
@@ -1,27 +1,28 @@
 import { post } from 'axios';
 
-const updateSellerZoop = async (zoopId, cpf, dayOfBirth, phone, category) => {
-  
-  const update = async () => {
-    await post(
-      `${process.env.PAY_URL}sellers-update?seller_id=${zoopId}`,
-        {
-          owner: {
-            taxpayer_id: cpf,
-            birthdate: dayOfBirth.split('/').reverse().join('-'),
-            phone_number: phone                
-          },
-          mcc: category
-        },
-        {
-          headers: {
-            Authorization: `${process.env.PAY_TOKEN}`,
-          },
-        }
-    );
-  };
+const updateSellerZoop = async (zoopId, cpf, dayOfBirth, phone, category, statementDescriptor) => {
 
-  await update();
+    const update = async () => {
+        await post(
+            `${process.env.PAY_URL}sellers-update?seller_id=${zoopId}`,
+            {
+                owner: {
+                    taxpayer_id: cpf,
+                    birthdate: dayOfBirth.split('/').reverse().join('-'),
+                    phone_number: phone
+                },
+                mcc: category,
+                statementDescriptor
+            },
+            {
+                headers: {
+                    Authorization: `${process.env.PAY_TOKEN}`,
+                },
+            }
+        );
+    };
+
+    await update();
 };
 
 export default updateSellerZoop;
