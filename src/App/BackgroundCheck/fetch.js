1ddbc9aff0307cdad47aa8957aa68fd67d6c12be
Wermeson Rocha <wermesonrocha2@gmail.com>
2021-06-08 10:33:22 -0300

    fix: Removal of monthly credit renewal

--- src/App/BackgroundCheck/fetch.js
+++ src/App/BackgroundCheck/fetch.js
@@ -1,35 +1,25 @@
-import { db } from '../../Firebase/index';
+import { db } from "../../Firebase/index"
 
 const fetch = (setIsLoading, setErrorLoading, docId, isCollaborator, ownerId, { setFreeRequests }) => {
-    const run = async () => {
-        try {
-            const refId = isCollaborator ? ownerId : docId;
-            const doc = await db.collection('suppliers').doc(refId).get();
-            let reqFree = 0;
-            if (doc.exists) {
-                const { backgroundCheckRequestsAvailable, backgroundCheckCurrentMonth, backgroundCheckCurrentYear } = doc.data();
-                const now = new Date();
-                // Requisição no mês corrente ainda
-                if (now.getMonth() <= backgroundCheckCurrentMonth && now.getFullYear() <= backgroundCheckCurrentYear) {
-                    reqFree = backgroundCheckRequestsAvailable;
-                }
-                // Requisição no mês diferente do corrente
-                else {
-                    await db.collection('suppliers').doc(refId).update({ backgroundCheckRequestsAvailable: 10, backgroundCheckCurrentMonth: now.getMonth(), backgroundCheckCurrentYear: now.getFullYear() });
-                    reqFree = 10;
-                }
-            }
-            setFreeRequests(reqFree);
-            setIsLoading(false);
-            setErrorLoading(false);
-        } catch (error) {
-            console.log(error);
-            if (error.customError) setCustomError(true);
-            else setErrorLoading(true);
-            setIsLoading(false);
-        }
+  const run = async () => {
+    try {
+      const refId = isCollaborator ? ownerId : docId
+      const doc = await db.collection("suppliers").doc(refId).get()
+      let reqFree = 0
+      if (doc.exists) {
+        const { backgroundCheckRequestsAvailable } = doc.data()
+        reqFree = backgroundCheckRequestsAvailable
+      }
+      setFreeRequests(reqFree)
+      setIsLoading(false)
+      setErrorLoading(false)
+    } catch (error) {
+      console.log(error)
+      setErrorLoading(true)
+      setIsLoading(false)
     }
-    run();
-};
+  }
+  run()
+}
 
-export default fetch;
+export default fetch
